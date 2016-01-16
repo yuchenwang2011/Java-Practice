8dd456d294f4ea1509ac10f826c4eadf6edab14f
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-01-16 17:04:15 -0500

    Optimized Space Complexity from O(M*N) to O(N)

--- 63.UniquePathsII.java
+++ 63.UniquePathsII.java
@@ -69,3 +69,55 @@ public class Solution {
         return dp[row-1][column-1];
     }
 }
+
+//second optimized method
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        //I got inspired by these two methods:
+        //https://leetcode.com/discuss/29816/short-java-solution
+        //https://leetcode.com/discuss/69907/ac-solution-with-little-modification-based-on-unique-path-i
+        //First implementation is the O(M*N) one and second O(N) one
+        if(obstacleGrid == null){
+           return 0;    
+        }
+        int row = obstacleGrid.length;
+        int column = obstacleGrid[0].length;
+        if (row * column == 0){
+            return 0;
+        }
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+            
+        }
+        int[][] dp = new int[row][column];
+        for(int i = 0; i < column; i++) {
+            if(obstacleGrid[0][i] == 1){
+//supoose ob[0][3]=1 then not only res[0][3]=0 but all grids behind it are also zeros(res[0][4], etc)
+                //dp[0][i] = 0;
+                break;
+            } else {
+                dp[0][i] = 1;
+            }
+        }
+        
+        for(int i = 0; i < row; i++){
+            if(obstacleGrid[i][0] == 1) {
+                //dp[i][0] = 0;
+                break;
+            } else {
+                dp[i][0]=1;
+            }
+        }
+        
+        for(int i =1; i < row; i++) {
+            for(int j = 1; j < column; j++){
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[row-1][column-1];
+    }
+}
