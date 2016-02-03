882ffdb396f54cf06e8a533c8f33c186985f9be9
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-02-02 23:34:48 -0500

    75 Sort Colors

---
+++ 75.SortColors.java
@@ -0,0 +1,48 @@
+75. Sort Colors My Submissions Question
+Total Accepted: 85495 Total Submissions: 251688 Difficulty: Medium
+Given an array with n objects colored red, white or blue, 
+sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Note:
+You are not suppose to use the library's sort function for this problem.
+
+click to show follow up.
+Follow up:
+A rather straight forward solution is a two-pass algorithm using counting sort.
+First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, 
+then 1's and followed by 2's.
+
+Could you come up with an one-pass algorithm using only constant space?
+
+Answer:
+public class Solution {
+    public void sortColors(int[] nums) {
+      //At first I wrote a quick sort, but i still have some problems
+      //https://leetcode.com/discuss/17000/share-my-one-pass-constant-space-10-line-solution
+      //https://leetcode.com/discuss/73086/concise-1-pass-java-solution
+      if(nums == null || nums.length ==0){
+          return;
+      }
+      
+      int zeroPointer = 0, twoPointer = nums.length-1;
+      for(int i = 0; i < nums.length; i++){
+          if(nums[i] == 0){
+              if(i != zeroPointer){
+                swap(nums,i,zeroPointer); 
+              }
+              zeroPointer++;
+          }
+          if(nums[i] == 2 && i < twoPointer){
+              swap(nums,i,twoPointer);
+              i--; twoPointer--; //wait 1 more round at current place, if it's still a 2, swap again
+          }
+      }
+      
+    }
+      public void swap(int[] nums, int first, int second){
+          int tmp = nums[first];
+          nums[first] = nums[second];
+          nums[second] = tmp;
+      }
+}
