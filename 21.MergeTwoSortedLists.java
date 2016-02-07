e4e0b8a33b94ceff2f098f4f2761039040ba9097
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-02-07 08:51:00 -0500

    21. Merge Two Sorted Lists using Recursion

--- 21.MergeTwoSortedLists.java
+++ 21.MergeTwoSortedLists.java
@@ -64,3 +64,18 @@ public class Solution {
       return head;
     }
 }
+
+//This method you have to 想象这是一串葡萄，每次返回后面的一串
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+      if(l1 == null) return l2;
+      if(l2 == null) return l1;
+      if(l1.val <= l2.val){
+          l1.next = mergeTwoLists(l1.next,l2);
+          return l1;
+      } else {
+          l2.next = mergeTwoLists(l1,l2.next);
+          return l2;
+      }
+    }
+}
