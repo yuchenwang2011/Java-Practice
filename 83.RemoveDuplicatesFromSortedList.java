33bddff56f42779d6f6fa98fbff7f2b3091e5cc9
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-02-08 21:12:51 -0500

    83. Remove Duplicates From Sorted List more intuitive

--- 83.RemoveDuplicatesFromSortedList.java
+++ 83.RemoveDuplicatesFromSortedList.java
@@ -46,5 +46,15 @@ public class Solution {
     }
 }
 
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null) return head;
+        head.next = deleteDuplicates(head.next);
+        if(head.val == head.next.val){
+            head = head.next;
+        }
+        return head;
+    }
+}
 
 
