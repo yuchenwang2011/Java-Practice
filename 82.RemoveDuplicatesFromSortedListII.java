2f18365dc1a32b59e328574c60ec3828dc28e88a
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-02-08 20:50:58 -0500

    82. Remove Duplicates From Sorted List II

---
+++ 82.RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,32 @@
+82. Remove Duplicates from Sorted List II My Submissions Question
+Total Accepted: 64492 Total Submissions: 245358 Difficulty: Medium
+Given a sorted linked list, delete all nodes that have duplicate numbers, 
+leaving only distinct numbers from the original list.
+
+For example,
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+
+Answer:
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return head;
+        if(head.next != null && head.val == head.next.val) {
+            while(head.next != null && head.val == head.next.val ){
+                head = head.next;
+            }
+            return deleteDuplicates(head.next);
+        } else {
+            head.next = deleteDuplicates(head.next);
+        }
+        return head;
+    }
+}
