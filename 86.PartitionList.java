b7a76c8c352d0505dfc289ac7c2656f3eac325b5
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-02-10 22:06:20 -0500

    86 Partition List using Recursion

---
+++ 86.PartitionList.java
@@ -0,0 +1,45 @@
+86. Partition List My Submissions Question
+Total Accepted: 58673 Total Submissions: 203634 Difficulty: Medium
+Given a linked list and a value x, 
+partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+For example,
+Given 1->4->3->2->5->2 and x = 3,
+return 1->2->2->4->3->5.
+
+Answer:
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    //I was wrong at first, someone solved my problem.
+    //https://leetcode.com/discuss/85513/whats-wrong-with-my-answer-java-using-recursion
+  public ListNode partition(ListNode head, int x) {
+    if(head == null || head.next == null) return head;
+    ListNode small = new ListNode(-1), big = new ListNode(-1);
+    ListNode[] result = new ListNode[]{small,big};
+    process(head,x,result);
+    result[1].next = null;
+    result[0].next = big.next;
+    return small.next;
+  }
+
+public void process(ListNode head,int x, ListNode[] result){
+    if(head == null) return;
+    if(head.val >= x){
+        result[1].next = head;
+        result[1] = result[1].next;
+    } else {
+        result[0].next = head;
+        result[0] = result[0].next;
+    }
+    process(head.next,x,result);
+  }
+}
