1d23de18a49c7c0fdebc1e2f3214824df8c31fc3
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-02-12 20:39:38 -0500

    24. Swap Node In Pairs using Recursion

--- 24.SwapNodeInPairs.java
+++ 24.SwapNodeInPairs.java
@@ -34,3 +34,13 @@ public class Solution {
       return newHead.next;
     }
 }
+
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode next = head.next;
+        head.next = swapPairs(head.next.next);
+        next.next = head;
+        return next;
+    }
+}
