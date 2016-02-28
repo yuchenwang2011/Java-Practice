1ab23e74845799b2399d27f0dff06bcc97c3c439
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-02-27 20:39:35 -0500

    230. Kth Smallest Element In a BST using My Own Idea

---
+++ 230.KthSmallestElementInaBST.java
@@ -0,0 +1,50 @@
+230. Kth Smallest Element in a BST My Submissions Question
+Total Accepted: 37549 Total Submissions: 103568 Difficulty: Medium
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+
+Hint:
+
+1.Try to utilize the property of a BST.
+2.What if you could modify the BST node's structure?
+3.The optimal runtime complexity is O(height of BST).
+
+Answer:
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+//This is my own idea, it definitly has some issues to work out.
+public class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        while(k >= 1){
+          int leftCount = countNode(root.left);
+          int rightCount = countNode(root.right);
+          if(k == leftCount+1) return root.val;
+          else if (k <= leftCount) root = root.left;
+          else {
+              root = root.right;
+              k = k - leftCount -1;
+          }
+        }
+        return root.val;
+    }
+    
+    public int countNode(TreeNode root){
+        if(root == null) return 0;
+        if(root.left == null && root.right == null) return 1;
+        return countNode(root.left) + countNode(root.right) +1; 
+    }
+}
+
+
