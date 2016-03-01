093eb3b80f7f01211ebf09fdce9084e624ab5d63
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-03-01 00:49:52 -0500

    199. Binary Tree Right Side View using DFS

--- 199.BinaryTreeRightSideView.java
+++ 199.BinaryTreeRightSideView.java
@@ -31,7 +31,6 @@ public class Solution {
         if(root == null) return result;
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.offer(root);
-        int level = 1;
         while(!queue.isEmpty()){
             int size = queue.size();
             for(int i = 0; i < size; i++){
@@ -40,10 +39,28 @@ public class Solution {
                 if(current.right != null) queue.offer(current.right);
                 if(i == size-1) {
                     result.add(current.val);
-                    level++;
                 }
             }
         }
         return result;
+    }
+}
+
+//Got inspired by this answer
+//https://leetcode.com/discuss/85020/java-easy-solution-beats-90%25-using-dfs
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(root == null) return result;
+        dfs(root,result,0);
+        return result;
+    }
+    public void dfs(TreeNode root, List<Integer> result, int level){
+        if(root == null) return;
+        if(level == result.size()) result.add(root.val);//0 level, size is 0; 1 level, size is 1; 2nd level, size is 2;
+        //so size and level each time increase 1 at the same time
+        //first right, if right null, then left
+        dfs(root.right, result, level+1);
+        dfs(root.left,result, level+1);
     }
 }
