fb7ec46c02579c5a45379b7c72c7a72a751c69f3
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-03-25 22:37:55 -0700

    71. Simplify Path using My Own Idea

---
+++ 71.SimplifyPath.java
@@ -0,0 +1,40 @@
+71. Simplify Path My Submissions Question
+Total Accepted: 48712 Total Submissions: 224714 Difficulty: Medium
+Given an absolute path for a file (Unix-style), simplify it.
+
+For example,
+path = "/home/", => "/home"
+path = "/a/./b/../../c/", => "/c"
+click to show corner cases.
+
+Corner Cases:
+Did you consider the case where path = "/../"?
+In this case, you should return "/".
+Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+In this case, you should ignore redundant slashes and return "/home/foo".
+
+Answer:
+public class Solution {
+    public String simplifyPath(String path) {
+        while(path.indexOf("//") >= 0){
+            path = path.trim().replaceAll("//","/");
+        }
+        if(path == null || path.length() == 0) return "/";
+        if(path.charAt(0) != '/') path = "/" + path;
+        String[] strings = path.split("/");
+        Deque<String> stack = new ArrayDeque<String>();
+        for(int i = 1; i < strings.length; i++){
+            if(strings[i].equals(".")) continue;
+            else if(strings[i].equals("..")) {
+                if(!stack.isEmpty()) stack.pop();
+            }
+            else stack.push("/" + strings[i]);
+        }
+        String result = "";
+        for(String str : stack){
+            result = str + result;
+        }
+        return result.length() == 0 ? "/" : result;
+    }
+}
+//Test case: "/a/./c/../b/", "/a/./b///../c/../././../d/..//../e/./f/./g/././//.//h///././/..///"
