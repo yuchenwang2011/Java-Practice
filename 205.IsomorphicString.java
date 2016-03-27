9d08e1cf5315f364b062b24711d6b06d50ca040d
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-03-27 15:55:53 -0400

    205. Isomorphic String

---
+++ 205.IsomorphicString.java
@@ -0,0 +1,37 @@
+205. Isomorphic Strings My Submissions Question
+Total Accepted: 51343 Total Submissions: 176037 Difficulty: Easy
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+For example,
+Given "egg", "add", return true.
+
+Given "foo", "bar", return false.
+
+Given "paper", "title", return true.
+
+Note:
+You may assume both s and t have the same length.
+
+Answer:
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s == null && t == null) return true;
+        if(s == null || t == null || s.length() != t.length()) return false;
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(map.containsKey(c)){
+                if(map.get(c) != c2) return false;
+            } else {
+                if(map.containsValue(c2)) return false;
+                map.put(c,c2);
+            }
+        }
+        return true;
+    }
+}
