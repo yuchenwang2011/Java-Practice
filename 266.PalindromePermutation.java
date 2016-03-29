8d5a5fde36d9213c0b6056b3e8714736aa75417b
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-03-29 15:22:07 -0700

    266. Palindrome Permutation My Own Idea

---
+++ 266.PalindromePermutation.java
@@ -0,0 +1,31 @@
+266. Palindrome Permutation My Submissions QuestionEditorial Solution
+Total Accepted: 10518 Total Submissions: 20930 Difficulty: Easy
+Given a string, determine if a permutation of the string could form a palindrome.
+
+For example,
+"code" -> False, "aab" -> True, "carerac" -> True.
+
+Hint:
+
+Consider the palindromes of odd vs even length. What difference do you notice?
+Count the frequency of each character.
+If each character occurs even number of times, then it must be a palindrome. 
+How about character which occurs odd number of times?
+
+Answer:
+public class Solution {
+    public boolean canPermutePalindrome(String s) {
+        if(s == null || s.length() == 0 ) return true;
+        HashMap<Character, Integer> map = new HashMap<Character,Integer>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(map.containsKey(c)) map.put(c,map.get(c) + 1);
+            else map.put(c,1);
+        }
+        int oddCount = 0;
+        for(char c : map.keySet()){
+            if(map.get(c) % 2 == 1) oddCount++;
+        }
+        return oddCount <= 1;
+    }
+}
