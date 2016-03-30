13f0a3a0fe211d28971078f3f4bce7597e071a50
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-03-30 17:32:49 -0400

    311. Sparse Matrix Multiplication Optimized

--- 311.SparseMatrixMultiplication.java
+++ 311.SparseMatrixMultiplication.java
@@ -45,3 +45,27 @@ public class Solution {
         return result;
     }
 }
+
+public class Solution {
+    public int[][] multiply(int[][] A, int[][] B) {
+        if(A == null || B == null) return null;
+        int aRow = A.length;
+        int aCol = A[0].length;
+        int bRow = B.length;
+        int bCol = B[0].length;
+        
+        if(aCol != bRow) return null;
+        int[][] C = new int[aRow][bCol];
+        
+        for(int i = 0; i <aRow; i++){
+            for(int k = 0; k < aCol; k++){
+                if(A[i][k] != 0){
+                    for(int j = 0; j < bCol; j++){
+                        if(B[k][j] != 0) C[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+        return C;
+    }
+}
