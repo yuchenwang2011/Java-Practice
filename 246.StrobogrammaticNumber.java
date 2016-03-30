493c9625090c023444f5e2d583e6e4cb482b343c
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-03-29 20:15:31 -0400

    246. Strobogrammatic Number using HashMap

---
+++ 246.StrobogrammaticNumber.java
@@ -0,0 +1,28 @@
+246. Strobogrammatic Number My Submissions QuestionEditorial Solution
+Total Accepted: 8514 Total Submissions: 23592 Difficulty: Easy
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+Write a function to determine if a number is strobogrammatic. The number is represented as a string.
+
+For example, the numbers "69", "88", and "818" are all strobogrammatic.
+
+Answer:
+public class Solution {
+    //Got inspired by these 2 answers
+    //https://leetcode.com/discuss/50594/4-lines-in-java
+    //https://leetcode.com/discuss/52273/accepted-java-solution
+    public boolean isStrobogrammatic(String num) {
+        if(num == null || num.length() == 0) return true;
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        map.put('0','0');
+        map.put('1','1');
+        map.put('6','9');
+        map.put('8','8');
+        map.put('9','6');
+        for(int i = 0 ; i < num.length(); i++){
+            if(map.containsKey(num.charAt(i)) && map.get(num.charAt(i)) == num.charAt(num.length() -1 - i)) continue;
+            else return false;
+        }
+        return true;
+    }
+}
