4c515af76118f1f4c0210ad71d06f2390a5b2446
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-04-05 18:15:06 -0400

    50. Pow(x,n) using Divide and Conquer

---
+++ 50.Pow(x,n).java
@@ -0,0 +1,16 @@
+50. Pow(x, n) My Submissions QuestionEditorial Solution
+Total Accepted: 86957 Total Submissions: 311869 Difficulty: Medium
+Implement pow(x, n).
+
+Answer:
+public class Solution {
+    //https://leetcode.com/discuss/52800/5-different-choices-when-talk-with-interviewers
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        double half = myPow(x, n/2);
+        double result = half * half;
+        if(n % 2 != 0) result = (n <0) ? result/x : result * x;
+        return result;
+    }
+}
