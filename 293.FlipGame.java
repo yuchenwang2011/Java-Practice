ada770b0c923556784811c61d022cc59b41cd66a
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-04-12 23:22:30 -0400

    Update 293.FlipGame.java

--- 293.FlipGame.java
+++ 293.FlipGame.java
@@ -1,6 +1,9 @@
 293. Flip Game My Submissions Question
 Total Accepted: 7244 Total Submissions: 14726 Difficulty: Easy
-You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
+You are playing the following Flip Game with your friend: 
+Given a string that contains only these two characters: + and -, 
+you and your friend take turns to flip two consecutive "++" into "--". 
+The game ends when a person can no longer make a move and therefore the other person will be the winner.
 
 Write a function to compute all possible states of the string after one valid move.
 
