9703782852027c1997aa0c2ceccfc0bc1dd22ae4
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-04-24 22:32:14 -0400

    191. Number of Bits using Method 2

--- 191.NumberOf1Bits.java
+++ 191.NumberOf1Bits.java
@@ -22,3 +22,15 @@ public class Solution {
         return ones;
     }
 }
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int ones = 0;
+        while(n != 0){
+            n = n & (n-1); //delete the right most bit 1
+            ones++;
+        }
+        return ones;
+    }
+}
