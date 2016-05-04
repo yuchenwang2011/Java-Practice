966bfe8a7ddfa3f3d5e7f0489c49d71a45b93f98
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-05-03 23:45:45 -0400

    Update 294.FlipGameII.java

--- 294.FlipGameII.java
+++ 294.FlipGameII.java
@@ -1,3 +1,18 @@
+294. Flip Game II   My Submissions QuestionEditorial Solution
+Total Accepted: 8894 Total Submissions: 21458 Difficulty: Medium
+You are playing the following Flip Game with your friend: 
+Given a string that contains only these two characters: + and -, 
+you and your friend take turns to flip two consecutive "++" into "--". 
+The game ends when a person can no longer make a move and therefore the other person will be the winner.
+
+Write a function to determine if the starting player can guarantee a win.
+
+For example, given s = "++++", return true. The starting player can guarantee a win by flipping the middle "++" to become "+--+".
+
+Follow up:
+Derive your algorithm's runtime complexity.
+
+Answer:
 public class Solution {
     //Got inspired by this answer
     //https://leetcode.com/discuss/64291/share-my-java-backtracking-solution
