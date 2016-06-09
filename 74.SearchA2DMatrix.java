e84b955519bfe572c03127105d94b9a4a93ac2f0
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-06-09 08:27:27 -0400

    Update 74.SearchA2DMatrix.java

--- 74.SearchA2DMatrix.java
+++ 74.SearchA2DMatrix.java
@@ -43,3 +43,22 @@ public class Solution {
         return result;
     }
 }
+
+//this is my second round answer
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) return false;
+        int row = matrix.length, col = matrix[0].length;
+        int start = 0, end = row * col - 1;
+        while(start + 1 < end){
+            int mid = start + (end - start) / 2;
+            int val = matrix[mid/col][mid % col];
+            if(val == target) return true;
+            else if (val > target) end = mid;
+            else start = mid;
+        }
+        if(matrix[start/col][start % col] == target) return true;
+        if(matrix[end/col][end % col] == target) return true;
+        return false;
+    }
+}
