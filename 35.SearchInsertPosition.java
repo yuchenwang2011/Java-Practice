b972b005c9a8ef7fb8956c7f6b76bcee498a07df
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-06-09 22:55:33 -0400

    Update 35.SearchInsertPosition.java

--- 35.SearchInsertPosition.java
+++ 35.SearchInsertPosition.java
@@ -38,3 +38,22 @@ public class Solution {
         //start increases and then equal to end, but still not match. finally more than end
     }
 }
+
+//this is my second round answer
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return 0;
+        if(target < nums[0]) return 0;
+        if(target > nums[nums.length - 1]) return nums.length;
+        int start = 0, end = nums.length - 1;
+        while(start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid] < target) start = mid;
+            else end = mid;
+        }
+        if(nums[start] == target) return start;
+        if(nums[end] == target) return end;
+        return start+1;
+    }
+}
