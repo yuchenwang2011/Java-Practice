02bb37d9354cf03f2d8c13a23d24e07d00efce84
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-06-12 00:38:44 -0400

    Update 27.RemoveElement.java

--- 27.RemoveElement.java
+++ 27.RemoveElement.java
@@ -29,3 +29,15 @@ public class Solution {
 }
 //[1,2,2] 2 expect [1], [1,2,3] 2 expects [1,3], [4,2,0,2,2,1,4,4,1,4,3,2] 4 expects [2,2,0,2,2,1,3,1]
 
+//this is my second round answer
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        if(nums == null || nums.length ==0) return 0;
+        int start = 0, end = nums.length - 1;
+        while(start <= end) {
+            if(nums[start] == val) nums[start] = nums[end--];
+            else start++;
+        }
+        return end + 1;
+    }
+}
