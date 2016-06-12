5ebcfb948ff23316789f2edbe3a70c0607b94c89
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-06-11 20:23:20 -0400

    Update 88.MergeSortedArray.java

--- 88.MergeSortedArray.java
+++ 88.MergeSortedArray.java
@@ -33,3 +33,19 @@ public class Solution {
         }
     }
 }
+
+//this is my second round answer
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(nums1 == null || nums1.length == 0 || m < 0 || n < 0 || nums2 == null || nums2.length == 0) return;
+        if(m + n > nums1.length) return;
+        int firstEnd = m - 1, secondEnd = n - 1, mergeEnd = m + n - 1;
+        while(firstEnd >= 0 && secondEnd >= 0) {
+            nums1[mergeEnd--] = (nums1[firstEnd] > nums2[secondEnd]) ? nums1[firstEnd--] : nums2[secondEnd--];
+        }
+        if(secondEnd < 0) return;
+        if(firstEnd < 0) {
+            while(secondEnd >= 0) nums1[mergeEnd--] = nums2[secondEnd--];
+        }
+    }
+}
