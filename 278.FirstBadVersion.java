c17a197f7d51e86be539cc913d5dd44c964c52e4
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-06-28 21:35:43 -0400

    Update 278.FirstBadVersion.java

--- 278.FirstBadVersion.java
+++ 278.FirstBadVersion.java
@@ -26,3 +26,18 @@ public class Solution extends VersionControl {
         return start;
     }
 }
+
+//this is my second round solution
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if(n <= 0) return 0;
+        int start = 1, end = n;
+        while(start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if(isBadVersion(mid)) end = mid;
+            else start = mid;
+        }
+        if(isBadVersion(start) == false) return start + 1;
+        else return start;
+    }
+}
