b4a2564da5c8be608ff05dac348d11a9596abafe
yuchenwang2011 <yuchen.wang2011@gmail.com>
2016-08-21 20:12:24 -0400

    Update 226.InvertBinaryTree.java

--- 226.InvertBinaryTree.java
+++ 226.InvertBinaryTree.java
@@ -43,3 +43,13 @@ public class Solution {
         return root;
     }
 }
+
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return root;
+        TreeNode tmp = invertTree(root.left);
+        root.left = invertTree(root.right);
+        root.right = tmp;
+        return root;
+    }
+}
