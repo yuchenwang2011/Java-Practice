0e7298a2d2a33f9151b1cee85193ecc38126e75e
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-03-31 17:21:18 -0400

    another solution of hard 3

--- 4.MedianOfTwoSortedArrays.java
+++ 4.MedianOfTwoSortedArrays.java
@@ -62,3 +62,40 @@ class Solution {
         return (nums[nums.length / 2 - 1] + nums[nums.length / 2 ]) / 2d;
     }
 }
+
+Solution2:
+Followed this tutorial: https://www.youtube.com/watch?v=LPFhl65R7ww
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        if(length1 > length2) return findMedianSortedArrays(nums2, nums1);
+        
+        int start = 0;
+        int end = length1;
+        
+        while(start <= end){
+            int partition1 = start + (end - start) / 2;
+            int partition2 = (length1 + length2 + 1) / 2 - partition1;
+            
+            int left1 = partition1 == 0? Integer.MIN_VALUE : nums1[partition1 - 1];
+            int right1 = partition1 == length1? Integer.MAX_VALUE : nums1[partition1];
+            
+            int left2 = partition2 == 0? Integer.MIN_VALUE : nums2[partition2 - 1];
+            int right2 = partition2 == length2? Integer.MAX_VALUE : nums2[partition2];
+            
+            if(left1 <= right2 && left2 <= right1) {
+                if((length1 + length2) %2 == 0) {
+                    return (Math.max(left1, left2) + Math.min(right1, right2)) / 2d;
+                } else {
+                    return (double) Math.max(left1, left2);
+                }
+            } else if (left1 > right2){
+                end = partition1 - 1;
+            } else {
+                start = partition1 + 1;
+            }
+        }
+        return 0d;
+    }
+}
