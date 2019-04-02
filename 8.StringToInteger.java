990281990a769999ba7b9d152355929ee95d8d19
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-04-01 21:19:03 -0400

    update question 8 answer

--- 8.StringToInteger.java
+++ 8.StringToInteger.java
@@ -7,30 +7,32 @@ Hint: Carefully consider all possible input cases. If you want a challenge, plea
 Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
 
 Answer: 
-public class Solution {
+class Solution {
     public int myAtoi(String str) {
-        if (str.length() == 0) {
-            return 0;
-        }
-        long result = 0; int tmp; int start = 0;int sign =1;
-
+        if(str == null || str.length() == 0) return 0;
         str = str.trim();
-        if(str.charAt(start) == '+') {
-          sign = 1;start++;
-        } else if (str.charAt(0) == '-') {
-          sign = -1;start++;
+        if(str.length() == 0) return 0;
+        int sign = 1;
+        int start = 0;
+        if(str.charAt(0) == '-'){
+            sign = -1 ;
+            start = 1;
+        } else if(str.charAt(0) == '+'){
+            sign = 1;
+            start = 1;
         }
-          for (int i = start; i< str.length();i++) {
-            tmp = str.charAt(i) - '0';
-            if (tmp < 0 || tmp > 9) {
-                break;
-            } else {
-              result = result * 10 + tmp; 
-              if (sign ==1 && result > Integer.MAX_VALUE) {return Integer.MAX_VALUE;}
-              if (sign ==-1 && sign * result < Integer.MIN_VALUE) {return Integer.MIN_VALUE;}
-            }
-          }
-          
+        
+        long result = 0;
+        
+        for(int i = start; i < str.length(); i++){
+            int tmp = str.charAt(i) - '0';
+            if(tmp < 0 || tmp > 9) break;
+            
+            result = result * 10 + tmp;
+            if(sign * result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if(sign * result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        }
+        
         return (int) result * sign;
     }
 }
