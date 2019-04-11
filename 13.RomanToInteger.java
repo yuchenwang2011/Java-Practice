74d1c9e2b84e3da725bdd37f1bc4ea101583b1df
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-04-10 23:16:17 -0400

    Update 13.RomanToInteger.java

--- 13.RomanToInteger.java
+++ 13.RomanToInteger.java
@@ -53,3 +53,50 @@ public class Solution {
         return count;
     }
 }
+
+
+Solution
+https://leetcode.com/problems/roman-to-integer/discuss/6529
+/My-solution-for-this-question-but-I-don't-know-is-there-any-easier-way
+    class Solution {
+    public int romanToInt(String s) {
+        long result = 0;
+        
+        if(s.indexOf("IV") != -1) result-=2;
+        if(s.indexOf("IX") != -1) result-=2;
+        if(s.indexOf("XL") != -1) result-=20;
+        if(s.indexOf("XC") != -1) result-=20;
+        if(s.indexOf("CD") != -1) result-=200;
+        if(s.indexOf("CM") != -1) result-=200;
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            switch(c){
+                case 'I': 
+                    result+=1;
+                    break;
+                case 'V':
+                    result+=5;
+                    break;
+                case 'X':
+                    result+=10;
+                    break;
+                case 'L':
+                    result+=50;
+                    break;
+                case 'C':
+                    result+=100;
+                    break;
+                case 'D':
+                    result+=500;
+                    break;
+                case 'M':
+                    result+=1000;
+                    break;
+                default:
+                    break;    
+            }
+        }
+        return (int) result;        
+    }
+}
