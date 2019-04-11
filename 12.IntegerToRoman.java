91c66ee24c91b9504be12a95596e70260ff3f8d6
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-04-10 22:33:27 -0400

    Update 12.IntegerToRoman.java

--- 12.IntegerToRoman.java
+++ 12.IntegerToRoman.java
@@ -5,49 +5,19 @@ Given an integer, convert it to a roman numeral.
 Input is guaranteed to be within the range from 1 to 3999.
 
 Answer:
-public class Solution {
-    //I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
+https://leetcode.com/problems/integer-to-roman/discuss/6310/My-java-solution-easy-to-understand
+class Solution {
     public String intToRoman(int num) {
-        if(num <= 0 || num > 3999) return null;
         String result = "";
-        int digit = 0;
-        while(num != 0){
-            int value = num%10;
-            digit++;
-            if(value >=1 && value <=3){
-                if(digit == 1)       result = addString(value, result, "I");
-                else if (digit == 2) result = addString(value, result, "X");
-                else if (digit == 3) result = addString(value, result, "C");
-                else                result = addString(value, result, "M");
-            }
-            if(value == 4){
-                if(digit == 1)       result = "IV" + result;
-                else if (digit == 2) result = "XL" + result;
-                else                result = "CD" + result;
-            }
-            if(value == 5){
-                if(digit == 1)       result = "V" + result;
-                else if (digit == 2) result = "L" + result;
-                else                result = "D" + result;
-            }
-            if(value >=6 && value <=8){
-                if(digit == 1)       result = "V" + addString(value%5, result, "I");
-                else if (digit == 2) result = "L" + addString(value%5, result, "X");
-                else                result = "D" + addString(value%5, result, "C");
-            }
-            if(value == 9){
-                if(digit == 1)       result = "IX" + result;
-                else if (digit == 2) result = "XC" + result;
-                else                 result = "CM" + result;
-            }
-            num = num/10;
-        }
-        return result;
-    }
+        if(num <= 0 || num > 3999) return result;
+        int[] nums = new int[]{1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] values = new String[]{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
     
-    public String addString(int value, String result, String letter){
-        for(int i = 0; i < value; i++) {
-            result = letter + result;
+        for(int i = 0; i < nums.length; i++){
+            while(num >= nums[i]){
+                num -= nums[i];
+                result += values[i];
+            }
         }
         return result;
     }
@@ -55,9 +25,8 @@ public class Solution {
 
 public class Solution {
     public String intToRoman(int num) {
+        https://leetcode.com/problems/integer-to-roman/discuss/6274/Simple-Solution
         //I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
-        //Got inspired by this link, it has many very good solutions in side.
-        //https://leetcode.com/discuss/1208/how-to-improve-code
         if (num <=0 || num > 3999) return null;
         String[] M = new String[]{"","M","MM","MMM"};
         String[] C = new String[]{"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
