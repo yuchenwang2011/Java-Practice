b6dfa42f69fb20c377a527bca324b40c8a7e9814
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-04-13 16:08:19 -0400

    Update 14.LongestCommonPrefix.java

--- 14.LongestCommonPrefix.java
+++ 14.LongestCommonPrefix.java
@@ -4,30 +4,6 @@ Write a function to find the longest common prefix string amongst an array of st
 
 Answer:
 public class Solution {
-    //To understand this question, go to first link
-    //https://leetcode.com/discuss/34186/what-does-longest-common-prefix-mean
-    public String longestCommonPrefix(String[] strs) {
-        if(strs == null || strs.length == 0) return "";
-        if(strs.length == 1) return strs[0];
-        int max = Integer.MAX_VALUE; 
-        for(int i = 0; i < strs.length-1; i++){
-            int tmp = -1;
-            String str1 = strs[i], str2= strs[i+1];
-            int length = Math.min(str1.length(), str2.length());
-            for(int j = 0; j < length; j++){
-                if(str1.charAt(j) == str2.charAt(j)) tmp = j;
-                else break;
-            }
-            if(tmp == -1) {max = -1; break;}
-            else max = Math.min(max, tmp);
-        }
-        if (max == -1) return "";
-        //System.out.println("abcd".substring(0,2));
-        return max == -1 ? "" : strs[0].substring(0,max+1);
-    }
-}
-
-public class Solution {
     //Got inspired by this answer
     //https://leetcode.com/discuss/20993/java-code-with-13-lines
     public String longestCommonPrefix(String[] strs) {
@@ -39,6 +15,22 @@ public class Solution {
             while(strs[i].indexOf(result) != 0) result = result.substring(0,result.length()-1);
         }
         return result;
+    }
+}
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0) {
+            return "";
+        }
+        int length = strs[0].length();
+        for(int i = 1; i < strs.length; i++){
+            while(length > 0 && strs[i].indexOf(strs[0].substring(0, length)) != 0){
+                length--;
+            }
+        }
+        
+        return strs[0].substring(0,length);
     }
 }
 
