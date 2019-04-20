0412c930b325751846a4f7f372322101be400c9a
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-04-20 12:55:21 -0400

    Update 28.Implement_strStr.java

--- 28.Implement_strStr.java
+++ 28.Implement_strStr.java
@@ -54,3 +54,27 @@ class Solution {
         return result;
     }
 }
+
+
+//My solution April 20, 2019
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle == null || needle.length() == 0) return 0;
+        if(haystack == null || haystack.length() == 0) return -1;
+        if(haystack.length() < needle.length()) return -1;
+        
+        char[] hays = haystack.toCharArray();
+        char[] nees = needle.toCharArray();
+        
+        for(int i = 0; i < hays.length - nees.length + 1; i++){
+            int j = i;
+            int k = 0;
+            while(k < nees.length && hays[j] == nees[k]){
+                j++;
+                k++;
+            }
+            if(k == nees.length) return i;
+        }
+        return -1;
+    }
+}
