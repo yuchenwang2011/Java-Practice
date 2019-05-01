a2d5043cb956f839b6d9de3a1f2152c808e07f29
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-01 11:16:22 -0400

    Update 34.SearchForARange.java

--- 34.SearchForARange.java
+++ 34.SearchForARange.java
@@ -10,6 +10,55 @@ For example,
 Given [5, 7, 7, 8, 8, 10] and target value 8,
 return [3, 4].
 
+//May1 2019: only need to remember this solution
+//https://www.youtube.com/watch?v=pZ7ypg3mU64
+public class Solution{
+    public int[] searchRange(int[] nums, int target){
+        int[] result = new int[]{-1,-1};
+        if(nums == null || nums.length == 0) return result;
+        result[0] = searchLeft(nums, target);
+        result[1] = searchRight(nums, target);
+        return result;
+    }
+
+    public int searchLeft(int[] nums, int target){
+        int start = 0;
+        int end = nums.length - 1;
+        while(start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if(target <= nums[mid]) {
+                end = mid;
+            } else {
+                start =mid;
+            }
+        }
+        if(nums[start] == target) return start;
+        if(nums[end] == target) return end;
+
+        return -1;
+    }
+
+    public int searchRight(int[] nums, int target){
+        int start = 0;
+        int end = nums.length - 1;
+        while(start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if(target < nums[mid]){
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+
+        if(nums[end] == target) return end;
+        if(nums[start] == target) return start;
+
+        return -1;
+    }
+}
+    
+    
+========================================
 Answer:
 public class Solution {
     public int[] searchRange(int[] nums, int target) {
