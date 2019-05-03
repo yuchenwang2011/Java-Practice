b99ed30065c1aff3ff5a074bd5bf65a887940281
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-02 22:19:03 -0400

    Update 43.MultiplyStrings.java

--- 43.MultiplyStrings.java
+++ 43.MultiplyStrings.java
@@ -32,3 +32,34 @@ public class Solution {
         return sb.length() == 0 ? "0" : sb.toString();
     }
 }
+
+//Second round solution
+class Solution {
+    public String multiply(String num1, String num2) {
+        if(num1 == null || num1.length() == 0 || num2 == null || num2.length() == 0) return "";
+        int[] result = new int[num1.length() + num2.length()];
+        
+        for(int i = num1.length() - 1; i >= 0; i--){
+            for(int j = num2.length() - 1; j >= 0; j--){
+                int product = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j;
+                int p2 = i + j + 1;
+                
+                //must review this part!!!!!
+                result[p2] += product;
+                result[p1] += result[p2] / 10;
+                result[p2] = result[p2] % 10;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < result.length; i++){
+            if(result[i] == 0 && sb.length() == 0){
+                continue;
+            }
+            sb.append(result[i]);
+        }
+        
+        return sb.length() == 0?  "0" : sb.toString();
+    }
+}
