5793e9781bd629131a8b17e31aaec01dc36a6fe1
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-05 14:09:51 -0400

    Update 103.BinaryTreeZigzagLevelOrderTraversal.java

--- 103.BinaryTreeZigzagLevelOrderTraversal.java
+++ 103.BinaryTreeZigzagLevelOrderTraversal.java
@@ -70,3 +70,30 @@ public class Solution {
         return result;
     }
 }
+
+
+//May5th 2019, my own recursive solution
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(root == null) return result;
+        
+        helper(root, result, 0);
+        return result;
+    }
+    
+    public void helper(TreeNode root, List<List<Integer>> result, int level){
+        if(root == null) return;
+        if(level >= result.size()){
+            result.add(new ArrayList<Integer>());
+        }
+        if(level % 2 == 0) {
+            result.get(level).add(root.val);   
+        } else {
+            result.get(level).add(0, root.val);
+        }
+        
+        helper(root.left, result, level + 1);
+        helper(root.right, result, level + 1);
+    }
+}
