bed72e7a4ecd63a6827ee5eccb5b43f4491da299
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-11 13:23:49 -0400

    Update 66.PlusOne.java

--- 66.PlusOne.java
+++ 66.PlusOne.java
@@ -4,71 +4,43 @@ Given a non-negative number represented as an array of digits, plus one to the n
 The digits are stored such that the most significant digit is at the head of the list.
 
 Answer:
-public class Solution {
-    public int[] plusOne(int[] digits) {
-      int length = digits.length;
-      if(length ==0 || digits == null) {
-        return new int[]{1};
-      }
-        
-      for(int i = length-1; i >=0 ; i--) {
-        if(digits[i] != 9){
-          digits[i] = digits[i]+1;
-          return digits;
-        } 
-        digits[i]=0;
-      }
-      int[] newDigits = new int[length+1];
-      newDigits[0] =1;
-      return newDigits;
-    }
-}
-
-//this is my second round answer, i don't know which one of them is better
-public class Solution{
-  public int[] plusOne(int[] digits){
-    if(digits == null || digits.length == 0) return new int[]{1};
-    int carry = 0, sum = 0;
-    for(int i = digits.length - 1; i >= 0; i--){
-      if(i == digits.length -1) {
-        sum = digits[i] + carry + 1;
-        digits[i] = sum % 10;
-        carry = sum / 10;
-        continue;
-      }
-      sum = digits[i] + carry;
-      digits[i] = sum % 10;
-      carry = sum / 10;
-    }
-    if(digits[0] == 0 && carry == 1) {
-      int[] result = new int[digits.length + 1];
-      result[0] = 1;
-      return result;
-    }
-    return digits;
-  }
-}
-
-//This is a combination of first method and second method
+//https://leetcode.com/problems/plus-one/discuss/24082/My-Simple-Java-Solution
 class Solution {
     public int[] plusOne(int[] digits) {
-        if(digits == null || digits.length == 0) return new int[]{1};
-        int carry = 0, sum = 0;
+        if(digits == null || digits.length == 0) return digits;
+        
         for(int i = digits.length - 1; i >= 0; i--){
-            if(i == digits.length - 1){
-                sum = 1 + digits[i];
-                digits[i] = sum % 10;
-                carry = sum / 10;
-                if(carry == 0) return digits;
-                continue;
+            if(digits[i] < 9) {
+                digits[i] ++;
+                return digits;
             }
-            sum = digits[i] + carry;
-            digits[i] = sum % 10;
-            carry = sum / 10;
-            if(carry == 0) return digits;
+            digits[i] = 0;
         }
-        int[] result = new int[digits.length+1];
+        
+        int[] result = new int[digits.length + 1];
         result[0] = 1;
+        
         return result;
+    }
+}
+
+
+//my own answer May11 2019
+class Solution {
+    public int[] plusOne(int[] digits) {
+        if(digits == null || digits.length == 0) return digits;
+        
+        int carry = 1;
+        for(int i = digits.length - 1; i >= 0; i--){
+            digits[i] = digits[i] + carry;
+            carry = digits[i] / 10;
+            digits[i] = digits[i] % 10;
+        }
+        if(carry == 1) {
+            int[] result = new int[1 + digits.length];
+            result[0] = 1;
+            return result;
+        }
+        return digits;
     }
 }
