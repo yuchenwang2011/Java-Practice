896db9cf5c2810ce2baafca55291e40eb024551f
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-11 23:18:07 -0400

    Update 69.Sqrt(x).java

--- 69.Sqrt(x).java
+++ 69.Sqrt(x).java
@@ -18,6 +18,26 @@ public class Solution {
         return start;
     }
 }
+//this is using template, use this one
+class Solution {
+    public int mySqrt(int x) {
+        if(x <= 0) return x;
+        int start = 1;
+        int end = x;
+        while(start + 1 < end){
+            int mid = start + (end - start) / 2;
+            if(mid == x / mid) return mid;
+            else if(mid < x / mid){
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if(start < x / start && end > x / end) return start;
+        return end;
+    }
+}
+
 
 public class Solution {
     //This answer uses Newton's method, but i don't think I can remember this
