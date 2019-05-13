8568742e81c9e6e1b538a871969d674508cccc3f
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-12 20:01:06 -0400

    Update 70.ClimbingStairs.java

--- 70.ClimbingStairs.java
+++ 70.ClimbingStairs.java
@@ -5,6 +5,23 @@ You are climbing a stair case. It takes n steps to reach to the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 Answer:
+//this is my own dp solution
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+        
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+    }
+}
+
 public class Solution {
     //This O(1) space solution maybe hard to understand, it's derived from the O(N) solution
     public int climbStairs(int n) {
