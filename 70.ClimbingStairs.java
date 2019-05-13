485f7c150b93161a72a253818f4bff1c79207512
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-12 20:05:46 -0400

    Update 70.ClimbingStairs.java

--- 70.ClimbingStairs.java
+++ 70.ClimbingStairs.java
@@ -5,6 +5,7 @@ You are climbing a stair case. It takes n steps to reach to the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
 Answer:
+//this question OJ doesn't support recursive method
 //this is my own dp solution
 class Solution {
     public int climbStairs(int n) {
