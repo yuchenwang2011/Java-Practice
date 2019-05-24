5b3697022e0e32c9d59fc2c78faf0dea00e7fbe4
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-23 20:24:21 -0400

    Update 87.ScrambleString.java

--- 87.ScrambleString.java
+++ 87.ScrambleString.java
@@ -79,6 +79,7 @@ class Solution {
             if(map[i] != 0) return false;
         }
 
+        //alert: here starts from 1!!
         for(int i = 1; i < s1.length(); i++){
             if(isScramble(s1.substring(0,i), s2.substring(0,i)) && isScramble(s1.substring(i), s2.substring(i))) return true;
             if(isScramble(s1.substring(0,i), s2.substring(s2.length() - i)) && isScramble(s1.substring(i), s2.substring(0,s2.length() - i))) return true;
