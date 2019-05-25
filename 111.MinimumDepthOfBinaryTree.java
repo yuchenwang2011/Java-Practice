0342423926cd59b24f85eb3432fb0bcf7d3a9e8f
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-25 00:37:28 -0400

    Update 111.MinimumDepthOfBinaryTree.java

--- 111.MinimumDepthOfBinaryTree.java
+++ 111.MinimumDepthOfBinaryTree.java
@@ -39,3 +39,25 @@ class Solution {
         return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
 }
+
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 1;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                if(node.left == null && node.right == null) return depth;
+                
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
