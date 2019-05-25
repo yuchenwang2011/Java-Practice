5a8353c6fd013e276db027eb615118e16233d05f
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-25 00:29:54 -0400

    Update 111.MinimumDepthOfBinaryTree.java

--- 111.MinimumDepthOfBinaryTree.java
+++ 111.MinimumDepthOfBinaryTree.java
@@ -27,3 +27,15 @@ public class Solution {
         else return Math.min(minDepth(root.left),minDepth(root.right))+1;
     }
 }
+
+
+//actually above solution can be simplified to this, but above it is easier to understand
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        if(root.left != null && root.right == null) return minDepth(root.left) + 1;
+        if(root.left == null && root.right != null) return minDepth(root.right) + 1;
+        
+        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+    }
+}
