9bac8e50dabbb3e859f8dd6699e61f8817117137
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-25 13:04:45 -0400

    Update 112.PathSum.java

--- 112.PathSum.java
+++ 112.PathSum.java
@@ -63,3 +63,27 @@ class Solution {
         return false;
     }
 }
+
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            if(node.left == null && node.right == null && node.val == sum) return true;
+            
+            if(node.right != null) {
+                node.right.val += node.val;
+                stack.push(node.right);
+            }
+            
+            if(node.left != null) {
+                node.left.val += node.val;
+                stack.push(node.left);
+            }
+        }
+        return false;
+    }
+}
