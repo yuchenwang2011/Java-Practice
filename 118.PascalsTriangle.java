7d2a97510d27d1ac4f35e2a11bb6e5af780986fd
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-25 23:23:17 -0400

    Update 118.PascalsTriangle.java

--- 118.PascalsTriangle.java
+++ 118.PascalsTriangle.java
@@ -11,7 +11,28 @@ Return
   [1,3,3,1],
  [1,4,6,4,1]
 ]
+
 Answer:
+//this answer is the best, from this link
+//https://leetcode.com/problems/pascals-triangle/discuss/38141/My-concise-solution-in-Java
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(numRows <= 0) return result;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < numRows; i++){
+            list.add(0,1);
+            //this will only run from size == 3
+            for(int j = 1; j < list.size() - 1; j++){
+                list.set(j, list.get(j) + list.get(j + 1));                
+            }
+            result.add(new ArrayList<>(list));
+        }
+        return result;
+    }
+}
+
+
 public class Solution {
     //not quite understand, also need to know how to use ArrayList set();
     public List<List<Integer>> generate(int numRows) {
