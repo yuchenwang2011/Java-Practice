a8fdd3aca66cac41786fbce6b55a110b6582b2ff
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-26 22:08:21 -0400

    Update 115.DistinctSubsequences.java

--- 115.DistinctSubsequences.java
+++ 115.DistinctSubsequences.java
@@ -1,16 +1,11 @@
 115. Distinct Subsequences
 Hard
 
-672
-
-36
-
-Favorite
-
-Share
 Given a string S and a string T, count the number of distinct subsequences of S which equals T.
-
-A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+A subsequence of a string is a new string which is formed from the original string 
+by deleting some (can be none) of the characters 
+without disturbing the relative positions of the remaining characters. 
+  (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
 
 Example 1:
 Input: S = "rabbbit", T = "rabbit"
