94fe5a11f3c63090af9b98e9bc345eb16bd4a4b8
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-27 13:43:51 -0400

    Update 122.BestTimeToBuyAndSellStockII.java

--- 122.BestTimeToBuyAndSellStockII.java
+++ 122.BestTimeToBuyAndSellStockII.java
@@ -9,34 +9,6 @@ However, you may not engage in multiple transactions at the same time
 (ie, you must sell the stock before you buy again).
 
 Anwer:
-public class Solution {
-    public int maxProfit(int[] prices) {
-        //my test case [7,5,8,1,30,100,4,2,7,10,11]
-        if (prices == null || prices.length < 2) {
-            return 0;
-        }
-        int lowPrice = prices[0], highPrice = prices[0];
-        int profit = 0;
-        for (int i = 0; i < prices.length ; i++) {
-            while (i < prices.length && prices[i] <= lowPrice) { //first find the local lowest price
-                lowPrice = prices[i];
-                i++;   
-            } // i should be 2 now, lowPrice = prices[1] = 5;
-            highPrice = lowPrice;
-            while (i < prices.length && prices[i] > highPrice) { 
-                //Second, find the local highest price after the lowest price try to sell;
-                highPrice = prices[i];
-                i++;
-            }
-            //now i=3 and highPrice = prices[2] = 8;
-            profit = profit + (highPrice - lowPrice);
-            lowPrice = highPrice;
-            i--; //Now we just loop through prices[2],8, but i is 3, next round it will be 4. We need check price[3]
-        }
-        return profit;
-    }
-}
-
 //this is my second round solution
 public class Solution{
   public int maxProfit(int[] nums){
@@ -52,4 +24,19 @@ public class Solution{
     }
     return result;
   }
+}
+
+//Solution May 27 2019.
+class Solution {
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        if(prices == null || prices.length < 2) return result;
+
+        for(int i = 1; i < prices.length; i++){
+            if(prices[i] > prices[i - 1]) {
+                result += prices[i] - prices[i - 1];
+            }
+        }
+        return result;
+    }
 }
