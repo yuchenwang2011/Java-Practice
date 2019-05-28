d00285656774c5e4fdd969a257f9456aff8b0019
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-28 00:27:19 -0400

    Update 127.WordLadder.java

--- 127.WordLadder.java
+++ 127.WordLadder.java
@@ -2,26 +2,83 @@
 Total Accepted: 84819
 Total Submissions: 430749
 Difficulty: Medium
-Given two words (beginWord and endWord), and a dictionary's word list, 
-find the length of shortest transformation sequence from beginWord to endWord, such that:
+Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
 
-Only one letter can be changed at a time
-Each intermediate word must exist in the word list
-For example,
-
-Given:
-beginWord = "hit"
-endWord = "cog"
-wordList = ["hot","dot","dog","lot","log"]
-As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
-return its length 5.
-
+Only one letter can be changed at a time.
+Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
 Note:
+
 Return 0 if there is no such transformation sequence.
 All words have the same length.
 All words contain only lowercase alphabetic characters.
+You may assume no duplicates in the word list.
+You may assume beginWord and endWord are non-empty and are not the same.
+Example 1:
+
+Input:
+beginWord = "hit",
+endWord = "cog",
+wordList = ["hot","dot","dog","lot","log","cog"]
+
+Output: 5
+
+Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.
+Example 2:
+
+Input:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log"]
+
+Output: 0
+
+Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
 
 Answer:
+//Alert!!2019 the question has changed. use this method
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if(beginWord == null || endWord == null || beginWord.length() != endWord.length() || wordList == null) return 0;
+        int distance = 1;
+        Set<String> set = new HashSet<>(wordList);
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                String each = queue.poll();
+                for(int j = 0; j < each.length(); j++){
+                    char[] chars = each.toCharArray();
+                    for(char c = 'a'; c <= 'z'; c++){
+                        chars[j] = c;
+                        String s = new String(chars);
+                        if(set.contains(s)){
+                            if(endWord.equals(s)) return distance + 1;
+                            queue.offer(s);
+                            set.remove(s);
+                        }
+                    }
+                }
+            }
+            distance++;
+        }
+        
+        return 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 public class Solution {
     //This explains how Dijkstra algorithm works for shortest path question
     //https://www.youtube.com/watch?v=WN3Rb9wVYDY
