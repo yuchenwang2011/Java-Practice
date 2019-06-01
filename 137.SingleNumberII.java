455a100ff84896134d1ab7eeab46a87b733fd3a7
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-06-01 18:30:37 -0400

    Update 137.SingleNumberII.java

--- 137.SingleNumberII.java
+++ 137.SingleNumberII.java
@@ -18,7 +18,7 @@ public class Solution {
             twos |= (ones & nums[i]);  //进位, 这里的ones&是为了提炼出nums里的第二次出现的1
             ones ^= nums[i];          //有1的就相当于进位了，没有1的就相当于提炼出1
             thress = ones & twos;      //如果ones和twos都是1，相当于1+2=3了，
-            ones &= ~threes;           //如果threes有一位是1，~就是0， 所以把ones和twos那一位清零了
+            ones &= ~threes;           //如果threes有一位是1也就是发现了一个出现3次的数，~就是0， 所以把ones和twos那一位清零了，重头再来
             twos &= ~threes;
         }
         return ones;                  //因为single number就出现一次，所以肯定是最后的ones
