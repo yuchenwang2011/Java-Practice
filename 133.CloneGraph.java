a9cdd7ba1ba952174ac3262d5963ee993493ab4f
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-05-31 23:46:56 -0400

    Update 133.CloneGraph.java

--- 133.CloneGraph.java
+++ 133.CloneGraph.java
@@ -22,39 +22,81 @@ Visually, the graph looks like the following:
     0 --- 2
          / \
          \_/
+              
+              
+              
+New version of question:
+133. Clone Graph
+Medium
+
+771
+
+830
+
+Favorite
+
+Share
+Given a reference of a node in a connected undirected graph, 
+return a deep copy (clone) of the graph. Each node in the graph 
+contains a val (int) and a list (List[Node]) of its neighbors.
+
+Example:
+1--2
+|  |
+4--3
+
+Input:
+{"$id":"1","neighbors":[{"$id":"2","neighbors":[{"$ref":"1"},{"$id":"3","neighbors":[{"$ref":"2"},{"$id":"4","neighbors":[{"$ref":"3"},{"$ref":"1"}],"val":4}],"val":3}],"val":2},{"$ref":"4"}],"val":1}
+
+Explanation:
+Node 1's value is 1, and it has two neighbors: Node 2 and 4.
+Node 2's value is 2, and it has two neighbors: Node 1 and 3.
+Node 3's value is 3, and it has two neighbors: Node 2 and 4.
+Node 4's value is 4, and it has two neighbors: Node 1 and 3.
+ 
+
+Note:
+
+The number of nodes will be between 1 and 100.
+The undirected graph is a simple graph, which means no repeated edges and no self-loops in the graph.
+Since the graph is undirected, if node p has node q as neighbor, then node q must have node p as neighbor too.
+You must return the copy of the given node as a reference to the cloned graph.
          
 Answer:
-/**
- * Definition for undirected graph.
- * class UndirectedGraphNode {
- *     int label;
- *     List<UndirectedGraphNode> neighbors;
- *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
- * };
- */
-//DFS solution:
-//https://leetcode.com/discuss/26988/depth-first-simple-java-solution
-public class Solution {
-    //First two link is explanation of the question, 
-    //https://leetcode.com/discuss/1361/graph-representation
-    //https://leetcode.com/discuss/961/wrong-sample-test-cases
-    
-    private HashMap<Integer, UndirectedGraphNode> map = new HashMap<>();
-    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-        return process(node);
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+//DFS
+class Solution {
+    Map<Node, Node> map = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        return helper(node);
     }
     
-    public UndirectedGraphNode process(UndirectedGraphNode node){
-        if(node == null) return null;
-        if(map.containsKey(node.label)) return map.get(node.label);
-        UndirectedGraphNode clone = new UndirectedGraphNode(node.label);
-        map.put(clone.label, clone);
-        for(UndirectedGraphNode neighbor : node.neighbors){
-            clone.neighbors.add(process(neighbor));
+    public Node helper(Node node){
+        if(node == null) return node;
+        if(map.containsKey(node)) return map.get(node);
+        Node dup = new Node(node.val);
+        dup.neighbors = new ArrayList<>();
+        map.put(node, dup);
+        for(Node nei : node.neighbors){
+            Node neiDup = helper(nei);
+            dup.neighbors.add(neiDup);
         }
-        return clone;
+        return dup;
     }
-    
 }
 
 //BFS
