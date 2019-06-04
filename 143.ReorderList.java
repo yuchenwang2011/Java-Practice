691ca2d4d0f8b75a9281c0a7511661e510955626
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-06-03 23:13:08 -0400

    Update 143.ReorderList.java

--- 143.ReorderList.java
+++ 143.ReorderList.java
@@ -57,3 +57,51 @@ public class Solution {
     }
 }
 
+//my own solution June 3 2019
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null) return;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode head2 = slow.next;
+        slow.next = null;
+        head2 = reverse(head2, null);
+        ListNode newHead = merge(head, head2);
+    }
+    
+    public ListNode reverse(ListNode head, ListNode prev){
+        if(head == null) return prev;
+        ListNode next = head.next;
+        head.next = prev;
+        return reverse(next, head);
+    }
+    
+    public ListNode merge(ListNode head1, ListNode head2){
+        if(head1 == null) return head2;
+        if(head2 == null) return head1;
+        ListNode headBackup = head1;
+        while(head1 != null && head2 != null){
+            ListNode next1 = head1.next;
+            ListNode next2 = head2.next;
+            head1.next = head2;
+            head2.next = next1;
+            
+            head1 = next1;
+            head2 = next2;
+        }
+        return headBackup;
+    }
+}
+
