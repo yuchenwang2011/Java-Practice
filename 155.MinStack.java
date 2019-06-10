9e6892339303bae484a9f635c74183132b83f743
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-06-09 21:13:38 -0400

    Update 155.MinStack.java

--- 155.MinStack.java
+++ 155.MinStack.java
@@ -41,3 +41,54 @@ class MinStack {
     }
 }
 
+
+//My answer June 2019 based on above solution but I feel easier to understand
+class MinStack {
+
+    private int min;
+    private Deque<Integer> stack;
+    
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        min = Integer.MAX_VALUE;
+    }
+    
+    public void push(int x) {
+        if(x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        if(stack.isEmpty()) {
+            return;
+        }
+        int val = stack.pop();
+        if(val == min) {
+            min = stack.pop();
+        }
+        if(stack.isEmpty()) min = Integer.MAX_VALUE;
+    }
+    
+    public int top() {
+        if(stack.isEmpty()) return min;
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
