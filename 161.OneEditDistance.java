e01d1834746ea2a7220de504efaa681fa59167b2
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-06-20 23:27:52 -0400

    Update 161.OneEditDistance.java

--- 161.OneEditDistance.java
+++ 161.OneEditDistance.java
@@ -4,28 +4,22 @@ Given two strings S and T, determine if they are both one edit distance apart.
 
 Answer:
 public class Solution {
-    //"1" "1" is false, "" "" is false, "1" "12" true, "1" "123" false, "1" "2" true, "" "1" true, "1" "23" false
-    //"ss1ss" "sss1s"
-    //I think this answer is best
-    //https://leetcode.com/discuss/71071/my-clear-java-solution-with-explanation
     public boolean isOneEditDistance(String s, String t) {
         if(s == null || t == null) return false;
-        if(s.length() == 0 && t.length() == 0) return false;
-        if(Math.abs(s.length() - t.length()) > 1) return false;
+        if(s.length() < t.length()) return isOneEditDistance(t, s);
         
-        for(int i = 0; i < Math.min(s.length(),t.length()); i++){
-            if(s.charAt(i) != t.charAt(i)) {
-                if(s.length() == t.length()){
-                    return s.substring(i+1).equals(t.substring(i+1));
-                } else if(s.length() > t.length()) {
-                    return s.substring(i+1).equals(t.substring(i));
-                } else {
-                    return s.substring(i).equals(t.substring(i+1));
-                }
+        for(int i = 0; i < t.length(); i++){
+            if(s.charAt(i) == t.charAt(i)) continue;
+            
+            if(s.length() == t.length()) {
+                return s.substring(i + 1).equals(t.substring(i + 1));
+            } else {
+                return s.substring(i + 1).equals(t.substring(i));
             }
         }
-        //so now all the chars are the same, once it has "ac" "a" case, we need to check the last digit by checking length
-        return Math.abs(s.length() - t.length()) ==1 ;
+        return s.length() - t.length() == 1;
     }
 }
 //Test case: "c" "c"
+//"1" "1" is false, "" "" is false, "1" "12" true, "1" "123" false, "1" "2" true, "" "1" true, "1" "23" false
+//"ss1ss" "sss1s"
