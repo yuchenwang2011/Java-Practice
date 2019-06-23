ab8770eebc45c67b641ecdd07bee02e63b8de8e5
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-06-23 11:45:31 -0400

    Update 43.MultiplyStrings.java

--- 43.MultiplyStrings.java
+++ 43.MultiplyStrings.java
@@ -5,34 +5,6 @@ Given two numbers represented as strings, return multiplication of the numbers a
 Note: The numbers can be arbitrarily large and are non-negative.
 
 Answer:
-public class Solution {
-    //Got inspired by first answer, but it's a little not clear, so refer second answer as well
-    //https://leetcode.com/discuss/71593/easiest-java-solution-with-graph-explanation
-    //https://leetcode.com/discuss/33951/ac-solution-in-java-with-explanation
-    public String multiply(String num1, String num2) {
-        if(num1 == null || num1.length() ==0 || num2 == null || num2.length() == 0) return "0";
-        int m = num1.length(); int n = num2.length();
-        int[] sum = new int[m+n];
-        
-        //i for num2, j for num1
-        for(int i = n-1; i >= 0 ; i--){
-            for(int j = m-1; j >= 0; j--){
-                int mul = (num1.charAt(j) - '0') * (num2.charAt(i) - '0');
-                int position1 = i + j; int position2 = i + j +1;
-                //do the calculation
-                int tmp = mul + sum[position2];
-                sum[position2] = tmp % 10;
-                sum[position1] = sum[position1] + tmp / 10;
-            }
-        }
-        StringBuilder sb = new StringBuilder();
-        for(int i : sum){
-            if( !(sb.length() == 0 && i == 0)) sb.append(i); //want to avoid the case, where first digit of sum is 0
-        }
-        return sb.length() == 0 ? "0" : sb.toString();
-    }
-}
-
 //Second round solution
 class Solution {
     public String multiply(String num1, String num2) {
