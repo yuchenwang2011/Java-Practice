10e43f705e86101fdb980f51160031dc4baaa190
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-06-30 14:28:37 -0400

    Update 167.TwoSumII.java

--- 1-300/151-200/167.TwoSumII.java
+++ 1-300/151-200/167.TwoSumII.java
@@ -8,26 +8,26 @@ Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 
 Answer:
-public class Solution {
+class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] result = new int[2];
-        int id1=0; int id2=numbers.length-1;
+        if(numbers == null || numbers.length < 2) return result;
         
-        if(numbers.length>=2 && numbers!=null){
-          while (id1 < id2) {
-            if(numbers[id1]+numbers[id2]<target){
-              id1++;
-            } else if (numbers[id1]+numbers[id2]>target) {
-              id2--;
+        int start = 0;
+        int end = numbers.length - 1;
+        
+        while(start < end){
+            int num = numbers[start] + numbers[end];
+            if(num == target) {
+                result[0] = start + 1;
+                result[1] = end + 1;
+                return result;
+            } else if(num < target){
+                start++;
             } else {
-              break;
+                end--;
             }
-          }
-          result[0]=id1+1;
-          result[1]=id2+1;
-          return result;
-        } else {
-          return null;
-        }
+        }     
+        return result;
     }
 }
