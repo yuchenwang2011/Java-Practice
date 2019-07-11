f8dac101583eaba071d66c348127361ff6df0510
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-07-10 22:39:36 -0400

    Update 172.FactorialTraillingZeros.java

--- 1-300/151-200/172.FactorialTraillingZeros.java
+++ 1-300/151-200/172.FactorialTraillingZeros.java
@@ -5,6 +5,12 @@ Given an integer n, return the number of trailing zeroes in n!.
 Note: Your solution should be in logarithmic time complexity.
 
 Answer:
+class Solution {
+    public int trailingZeroes(int n) {
+        return n == 0 ? 0 : n / 5 +  trailingZeroes(n / 5);
+    }
+}
+
 //think this way: 12345,678910. only 5 x 2 makes a zero. and there are way more 2s than 5s.
 //so actually just to count how many 5s there are. so 5, 10, 15, 20, 25, 30....125.
 //they have 1,1,1,1,2 number of fives, every 5 number has an extra 5.
