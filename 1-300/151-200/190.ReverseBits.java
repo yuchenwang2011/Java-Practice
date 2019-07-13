c37ad276bf3ff3fcf30bc97780899563084ee3d1
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-07-13 17:53:21 -0400

    Update 190.ReverseBits.java

--- 1-300/151-200/190.ReverseBits.java
+++ 1-300/151-200/190.ReverseBits.java
@@ -17,6 +17,7 @@ public class Solution {
         int result = 0;
         for(int i = 0; i < 32; i++){
             result = (result << 1 ) |  (n & 1);
+            //here use >> is also ok
             n = n >>> 1;
         }
         return result;
