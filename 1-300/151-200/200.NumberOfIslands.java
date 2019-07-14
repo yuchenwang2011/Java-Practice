d222ddd845fc9572d63815520b27cf3844b6da58
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-07-14 19:21:54 -0400

    Update 200.NumberOfIslands.java

--- 1-300/151-200/200.NumberOfIslands.java
+++ 1-300/151-200/200.NumberOfIslands.java
@@ -55,50 +55,54 @@ class Solution {
 }
 
 //BFS:
-public int numIslands(char[][] grid) {
-    int count=0;
-    for(int i=0;i<grid.length;i++)
-        for(int j=0;j<grid[0].length;j++){
-            if(grid[i][j]=='1'){
-                bfsFill(grid,i,j);
-                count++;
+class Solution {
+    public int numIslands(char[][] grid) {
+        int result = 0;
+        if(grid == null || grid.length == 0 || grid[0].length == 0) return result;
+
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1') {
+                    result++;
+                    helper(grid, i, j);
+                }
             }
         }
-    return count;
-}
-private void bfsFill(char[][] grid,int x, int y){
-    grid[x][y]='0';
-    int n = grid.length;
-    int m = grid[0].length;
-    LinkedList<Integer> queue = new LinkedList<Integer>();  
-    int code = x*m+y;  
-    queue.offer(code);  
-    while(!queue.isEmpty())  
-    {  
-        code = queue.poll();  
-        int i = code/m;  
-        int j = code%m;  
-        if(i>0 && grid[i-1][j]=='1')    //search upward and mark adjacent '1's as '0'.
-        {  
-            queue.offer((i-1)*m+j);  
-            grid[i-1][j]='0';  
-        }  
-        if(i<n-1 && grid[i+1][j]=='1')  //down
-        {  
-            queue.offer((i+1)*m+j);  
-            grid[i+1][j]='0';  
-        }  
-        if(j>0 && grid[i][j-1]=='1')  //left
-        {  
-            queue.offer(i*m+j-1);  
-            grid[i][j-1]='0';  
-        }  
-        if(j<m-1 && grid[i][j+1]=='1')  //right
-        {  
-            queue.offer(i*m+j+1);  
-            grid[i][j+1]='0';  
+        
+        return result;
+    }
+    
+    public void helper(char[][] grid, int x, int y){
+        grid[x][y] = '0';
+        int row = grid.length;
+        int col = grid[0].length;
+        
+        int elementId = x * col + y;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(elementId);
+        
+        while(!queue.isEmpty()){
+            int id = queue.poll();
+            int i = id / col;
+            int j = id % col;
+            if(i > 0 && grid[i - 1][j] == '1') {
+                queue.offer((i - 1) * col + j);
+                grid[i - 1][j] = '0';
+            }
+            if(i < row - 1 && grid[i + 1][j] == '1'){
+                queue.offer((i + 1) * col + j);
+                grid[i + 1][j] = '0';
+            }
+            if(j > 0 && grid[i][j - 1] == '1'){
+                queue.offer(i * col + j - 1);
+                grid[i][j - 1] = '0';
+            }
+            if(j < col - 1 && grid[i][j + 1] == '1'){
+                queue.offer(i * col + j + 1);
+                grid[i][j + 1] = '0';
+            }
         }
-    } 
+    }
 }
 
 
