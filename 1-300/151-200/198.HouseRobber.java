d5d2b27159293b6ff002a50f95164c87ae9d6616
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-07-13 20:58:50 -0400

    Update 198.HouseRobber.java

--- 1-300/151-200/198.HouseRobber.java
+++ 1-300/151-200/198.HouseRobber.java
@@ -27,3 +27,20 @@ public class Solution {
         return dp[nums.length - 1];
     }
 }
+
+class Solution {
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        
+        int first = nums[0];
+        int second = Math.max(first, nums[1]);
+        int result = Math.max(first, second);
+        for(int i = 2; i < nums.length; i++){
+            result = Math.max(first + nums[i], second + 0);
+            first = second;
+            second = result;
+        }
+        return result;
+    }
+}
