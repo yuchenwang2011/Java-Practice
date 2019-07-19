87c509be418761ef194f8c9cd73ac44052c37c68
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-07-19 17:05:31 -0400

    Update 209.MinimumSizeSubarraySum.java

--- 1-300/201-250/209.MinimumSizeSubarraySum.java
+++ 1-300/201-250/209.MinimumSizeSubarraySum.java
@@ -11,16 +11,9 @@ If you have figured out the O(n) solution,
 try coding another solution of which the time complexity is O(n log n).
 
 Answer:
-//First O(N) method:
+//two pointer question
 public class Solution {
     public int minSubArrayLen(int s, int[] nums) {
-        //The O(N) solution: two pointer is actually a window
-        //https://leetcode.com/discuss/74489/1-ms-o-n-time-solution-in-java-dynamic-sliding-window
-        //https://leetcode.com/discuss/67345/java-concise-solution-with-o-n-complexity
-        //https://leetcode.com/discuss/45449/accepted-clean-java-o-n-solution-two-pointers
-        
-        //The O(NlogN) solution:
-        //https://leetcode.com/discuss/35335/o-nlgn-is-not-that-easy-here-is-my-java-code
         if(nums == null || nums.length == 0){
             return 0;
         }
