18cf49f2c5d726280acb749bee60a02ba47377b6
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-07-21 15:18:54 -0400

    Update 216.CombinationSumIII.java

--- 1-300/201-250/216.CombinationSumIII.java
+++ 1-300/201-250/216.CombinationSumIII.java
@@ -16,27 +16,24 @@ Output:
 [[1,2,6], [1,3,5], [2,3,4]]
 
 Answer:
-public class Solution {
-    public List<List<Integer>> combinationSum3(int k, int target) {
-       List<List<Integer>> result = new ArrayList<List<Integer>>();
-       if(k < 0 || k > 9 || target <0){
-           return result;
-       }
-       List<Integer> tmp = new ArrayList<Integer>();
-       dfs(k,target, result, tmp, 0);
-       return result;
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(k <= 0 || n <= 0) return result;
+        helper(result, k, n, new ArrayList<Integer>(), 1);
+        return result;
     }
     
-    public void dfs(int k,int target,List<List<Integer>> result,List<Integer> tmp,int idx){
-      if(target == 0 && tmp.size() == k){
-          result.add(new ArrayList<Integer>(tmp));
-          return;
-      }   
-      if(target < 0 || tmp.size() >= k) return;
-      for(int i = idx; i < 9; i++){
-          tmp.add(i+1);
-          dfs(k,target-i-1,result,tmp,i+1);
-          tmp.remove(tmp.size()-1);
-      }
+    public void helper(List<List<Integer>> result, int k, int n, List<Integer> tmp, int idx){
+        if(k == 0 && n == 0) {
+            result.add(new ArrayList<>(tmp));
+            return;
+        }
+        if(k <= 0 || n <= 0 || idx > 9) return;
+        for(int i = idx; i <= 9; i++){
+            tmp.add(i);
+            helper(result, k - 1, n - i, tmp, i + 1);
+            tmp.remove(tmp.size() - 1);
+        }
     }
 }
