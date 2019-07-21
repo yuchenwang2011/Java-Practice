8ac04e947c69c58cdaacd895ca1f711e12214a68
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-07-21 13:12:44 -0400

    Update 214.ShortestPalindrome.java

--- 1-300/201-250/214.ShortestPalindrome.java
+++ 1-300/201-250/214.ShortestPalindrome.java
@@ -13,3 +13,27 @@ Input: "abcd"
 Output: "dcbabcd"
 
 Answer:
+class Solution {
+    public String shortestPalindrome(String s) {
+        if(s == null || s.length() == 0) return s;
+        
+        int start = 0;
+        int endBackUp = s.length() - 1;
+        int end = endBackUp;
+        while(start < end){
+            if(s.charAt(start) == s.charAt(end)) {
+                start++;
+                end--;
+            } else {
+                //means last round, start to last end doesn't work
+                //so startover and try test one letter before end of string
+                start = 0;
+                endBackUp--;
+                end = endBackUp;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        return sb.append(s.substring(endBackUp + 1)).reverse().append(s).toString();
+    }
+}
