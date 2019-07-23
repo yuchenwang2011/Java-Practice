fcd9f05979a444beaf0d0f8577f4d940b9bb0c18
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-07-23 19:46:01 -0400

    Update 225.ImplementStackUsingQueues.java

--- 1-300/201-250/225.ImplementStackUsingQueues.java
+++ 1-300/201-250/225.ImplementStackUsingQueues.java
@@ -16,29 +16,53 @@ You may assume that all operations are valid (for example,
 no pop or top operations will be called on an empty stack).
 
 Answer:
+//第一个方法是新的，用一个queue，更好一些。
+//第二个方法是当年的好理解的，用两个queue，倒来倒去
 class MyStack {
-    //There are also many good implementations, but I don't have time this round
+    Queue<Integer> queue;
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        queue.offer(x);
+        int size = queue.size() - 1;
+        for(int i = 0; i < size; i++){
+            queue.offer(queue.poll());
+        }
+    }
+    
+    public int pop() {
+        return queue.poll();
+    }
+    
+    public int top() {
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+
+class MyStack {
     private Queue<Integer> queue = new LinkedList<Integer>();
     private Queue<Integer> tmpQueue = new LinkedList<Integer>();
     
-    // Push element x onto stack.
     public void push(int x) {
         while(!queue.isEmpty()) tmpQueue.offer(queue.poll());
         queue.offer(x);
         while(!tmpQueue.isEmpty()) queue.offer(tmpQueue.poll());
     }
-
-    // Removes the element on top of the stack.
     public void pop() {
         queue.poll();
     }
 
-    // Get the top element.
     public int top() {
         return queue.peek();
     }
 
-    // Return whether the stack is empty.
     public boolean empty() {
         return queue.isEmpty();
     }
