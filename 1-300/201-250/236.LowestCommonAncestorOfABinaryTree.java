b035a961b3c80ba6a375ac0fe1f4a6c174fc1138
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-07-28 17:38:30 -0400

    Update 236.LowestCommonAncestorOfABinaryTree.java

--- 1-300/201-250/236.LowestCommonAncestorOfABinaryTree.java
+++ 1-300/201-250/236.LowestCommonAncestorOfABinaryTree.java
@@ -30,6 +30,7 @@ Answer:
 //https://leetcode.com/discuss/83644/share-my-5-line-java-code-with-brief-explanation
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //其实这句话是最最最重要的，其实找的就是当循环到p和q的时候，返回的就是p和q
         if(root == null || root == p || root == q) return root;
         TreeNode left = lowestCommonAncestor(root.left,p,q);
         TreeNode right = lowestCommonAncestor(root.right,p,q);
