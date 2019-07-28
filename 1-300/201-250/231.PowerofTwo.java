8ab33bd56e9e70f7726010db9eab3f146f3b8352
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-07-27 20:47:44 -0400

    Update 231.PowerofTwo.java

--- 1-300/201-250/231.PowerofTwo.java
+++ 1-300/201-250/231.PowerofTwo.java
@@ -3,19 +3,18 @@ Total Accepted: 65098 Total Submissions: 180545 Difficulty: Easy
 Given an integer, write a function to determine if it is a power of two.
 
 Answer:
-//First one is my own silly answer, second one is really smart
-public class Solution {
+class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n <=0) return false;
-        while(n % 2 == 0 && n != 1){
+        if(n <= 0) return false;
+        while(n != 1){
+            if(n % 2 != 0) return false;
             n = n >> 1;
         }
-        return n == 1;
+        return true;
     }
 }
 
 public class Solution {
-    //https://leetcode.com/discuss/43875/using-n%26-n-1-trick
     public boolean isPowerOfTwo(int n) {
         //because if is power of two, it should has only 1 zero in the start of the number, in binary
         return n > 0 && (n & (n-1) ) == 0;
