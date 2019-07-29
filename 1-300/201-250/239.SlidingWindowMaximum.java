c02056f780c7e725d82fd95072c25e9f944429c8
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-07-29 00:17:54 -0400

    Create 239.SlidingWindowMaximum.java

---
+++ 1-300/201-250/239.SlidingWindowMaximum.java
@@ -0,0 +1,49 @@
+239. Sliding Window Maximum
+Hard
+Given an array nums, there is a sliding window of size k 
+which is moving from the very left of the array to the very right. 
+You can only see the k numbers in the window. 
+Each time the sliding window moves right by one position. Return the max sliding window.
+
+Example:
+Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+Output: [3,3,5,5,6,7] 
+
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+ 
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
+Could you solve it in linear time?
+
+Answer:
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0) {
+            return nums;
+        }
+        int[] result = new int[n - k + 1];
+        LinkedList<Integer> dq = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (!dq.isEmpty() && dq.peek() == i - k) {
+                dq.poll();
+            }
+            while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()]) {
+                dq.pollLast();
+            }
+            dq.offer(i);
+            if (i - k + 1 >= 0) {
+                result[i - k + 1] = nums[dq.peek()];
+            }
+        }
+        return result;
+    }
+}
