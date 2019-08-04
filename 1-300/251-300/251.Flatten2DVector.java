236bfd5253a6433645c19cb8a7eab9131d4395b9
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-03 21:05:03 -0400

    Create 251.Flatten2DVector.java

---
+++ 1-300/251-300/251.Flatten2DVector.java
@@ -0,0 +1,39 @@
+251. Flatten 2D Vector
+Medium
+
+Design and implement an iterator to flatten a 2d vector. 
+It should support the following operations: next and hasNext.
+
+Example:
+Vector2D iterator = new Vector2D([[1,2],[3],[4]]);
+
+iterator.next(); // return 1
+iterator.next(); // return 2
+iterator.next(); // return 3
+iterator.hasNext(); // return true
+iterator.hasNext(); // return true
+iterator.next(); // return 4
+iterator.hasNext(); // return false
+ 
+
+Notes:
+Please remember to RESET your class variables declared in Vector2D, 
+as static/class variables are persisted across multiple test cases. Please see here for more details.
+You may assume that next() call will always be valid, that is, 
+there will be at least a next element in the 2d vector when next() is called.
+
+Hint:
+1. How many variables do you need to keep track?
+2. Two variables is all you need. Try with x and y.
+3. Beware of empty rows. It could be the first few rows.
+4. To write correct code, think about the invariant to maintain. What is it?
+5. The invariant is x and y must always point to a valid point in the 2d vector. 
+   Should you maintain your invariant ahead of time or right when you need it?
+6. Not sure? Think about how you would implement hasNext(). Which is more complex?
+7. Common logic in two different places should be refactored into a common method.
+ 
+ 
+Follow up:
+As an added challenge, try to code it using only iterators in C++ or iterators in Java.
+
+Answer:
