f0632f884209e3fdbdb2e95a6b0a60b33f5782f9
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-04 02:38:14 -0400

    Update 260.SingleNumberIII.java

--- 1-300/251-300/260.SingleNumberIII.java
+++ 1-300/251-300/260.SingleNumberIII.java
@@ -22,8 +22,14 @@ public class Solution {
         for(int num : nums){
             diff ^= num;
         }
+        //In the first pass, we XOR all elements in the array, and get the XOR of the two numbers we need to find. 
+        //Note that since the two numbers are distinct, so there must be a set bit (that is, the bit with value '1') in the XOR result.
+        //Find out an arbitrary set bit (for example, the rightmost set bit).
         diff &= -diff; //get the right most 1, use a number to try yourself
         for(int num : nums){
+            //In the second pass, we divide all numbers into two groups, one with the aforementioned bit set, 
+            //another with the aforementinoed bit unset. Two different numbers we need to find must fall into thte two distrinct groups. 
+            //XOR numbers in each group, we can find a number in either group.
             if((num & diff) == 0) result[0] ^= num;
             else result[1] ^= num;
         }
