ef73b13b27881ae5213242e3e6bc59edaa0f0863
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-03 21:45:08 -0400

    Update 252.MeetingRooms.java

--- 1-300/251-300/252.MeetingRooms.java
+++ 1-300/251-300/252.MeetingRooms.java
@@ -8,28 +8,14 @@ Given [[0, 30],[5, 10],[15, 20]],
 return false.
 
 Answer:
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
-public class Solution {
-    //Got inspired by these 2 answers
-    //********Remember the usage of Comparator!!!!
-    //https://leetcode.com/discuss/50912/ac-clean-java-solution
-    public boolean canAttendMeetings(Interval[] intervals) {
-        if(intervals == null || intervals.length == 0) return true;
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        if(intervals == null || intervals.length == 0 || intervals[0].length == 0) return true;
         
-        Arrays.sort(intervals, new Comparator<Interval>(){
-            public int compare(Interval a, Interval b){return a.start - b.start;}
-        });
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         
         for(int i = 0; i < intervals.length - 1; i++){
-            if(intervals[i].end > intervals[i+1].start ) return false;
+            if(intervals[i][1] > intervals[i + 1][0]) return false;
         }
         
         return true;
