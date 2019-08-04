1804d064bd0d1ab3498a7bcd9f6fe05542413864
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-04 01:17:47 -0400

    Update 257.BinaryTreePaths.java

--- 1-300/251-300/257.BinaryTreePaths.java
+++ 1-300/251-300/257.BinaryTreePaths.java
@@ -16,55 +16,20 @@ Credits:
 Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
 
 Answer:
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-//The first answer is a very good DFS template, second answer is my first intention but i don't know StringBuilder class
-//https://leetcode.com/discuss/79797/2ms-java-recursive-solution-with-explaination
-//https://leetcode.com/discuss/55581/solution-using-stringbuilder-instead-string-manipulation
-public class Solution {
+class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> result = new ArrayList<String>();
-        process(root,result,"");
+        List<String> result = new ArrayList<>();
+        if(root == null) return result;
+        helper(result, "", root);
         return result;
     }
     
-    public void process(TreeNode root, List<String> result, String tmp){
-        if(root == null) return;
-        if(root.left == null && root.right == null){
-            result.add(tmp + root.val);
-            return; //only add to final result when the current node is a leaf
-        }
-        process(root.left, result, tmp + root.val + "->");
-        process(root.right, result, tmp + root.val + "->");
-    }
-}
-
-public class Solution {
-    public List<String> binaryTreePaths(TreeNode root){
-        List<String> result = new ArrayList<String>();
-        StringBuilder tmp = new StringBuilder();
-        process(root,result,tmp);
-        return result;
-    }
-    public void process(TreeNode root, List<String> result, StringBuilder tmp){
-        if(root == null) return;
-        int size = tmp.length();
+    public void helper(List<String> result, String tmp, TreeNode root){
         if(root.left == null && root.right == null) {
-            tmp.append(root.val);
-            result.add(tmp.toString());
-            tmp.delete(size,tmp.length());
-            return;
-        }
-        tmp.append(root.val + "->");
-        process(root.left,result,tmp);
-        process(root.right,result,tmp);
-        tmp.delete(size,tmp.length());
+            result.add(tmp + root.val);
+            return;   
+        }    
+        if(root.left != null)  helper(result, tmp + root.val + "->", root.left);
+        if(root.right != null) helper(result, tmp + root.val + "->", root.right);
     }
 }
