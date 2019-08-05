23c5cdea838a9161e68b394e6bf64916b387fe55
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-05 14:50:32 -0400

    Update 264.UglyNumberII.java

--- 1-300/251-300/264.UglyNumberII.java
+++ 1-300/251-300/264.UglyNumberII.java
@@ -11,31 +11,27 @@ Hint:
 
 1. The naive approach is to call isUgly for every number until you reach the nth one. 
 Most numbers are not ugly. Try to focus your effort on generating only the ugly ones.
-
 2. An ugly number must be multiplied by either 2, 3, or 5 from a smaller ugly number.
-
 3. The key is how to maintain the order of the ugly numbers. 
 Try a similar approach of merging from three sorted lists: L1, L2, and L3.
-
 4. Assume you have Uk, the kth ugly number. Then Uk+1 must be Min(L1 * 2, L2 * 3, L3 * 5).
 
 Answer:
-public class Solution {
-    //Got inspired by this answer:
-    //https://leetcode.com/discuss/52716/o-n-java-solution
+class Solution {
     public int nthUglyNumber(int n) {
-        if(n <= 0) return 0;
+        if(n <= 1) return n;
         int[] ugly = new int[n];
         ugly[0] = 1;
-        int index2 = 0, index3 = 0, index5 = 0;
-        int factor2 = 2, factor3 = 3, factor5 = 5;
+        
+        int idx2 = 0;
+        int idx3 = 0;
+        int idx5 = 0;
         for(int i = 1; i < n; i++){
-            int min = Math.min(factor5, Math.min(factor2, factor3));
-            ugly[i] = min;
-            if(factor2 == min) factor2 = 2 * ugly[++index2];
-            if(factor3 == min) factor3 = 3 * ugly[++index3];
-            if(factor5 == min) factor5 = 5 * ugly[++index5];
+            ugly[i] = Math.min(ugly[idx2] * 2, Math.min(ugly[idx3] * 3, ugly[idx5] * 5));
+            if(ugly[i] == ugly[idx2] * 2) idx2++;
+            if(ugly[i] == ugly[idx3] * 3) idx3++;
+            if(ugly[i] == ugly[idx5] * 5) idx5++;
         }
-        return ugly[n-1];
+        return ugly[n - 1];
     }
 }
