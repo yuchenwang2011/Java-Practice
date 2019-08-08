ebfa61c9f7b2146c1d662f7fcc918185f8b14d88
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-08 15:06:06 -0400

    Update 279.PerfectSquares.java

--- 1-300/251-300/279.PerfectSquares.java
+++ 1-300/251-300/279.PerfectSquares.java
@@ -22,20 +22,18 @@ Answer:
 //        = 2
 // dp[n] = Min{ dp[n - i*i] + 1 },  n - i*i >=0 && i >= 1
 
-public class Solution {
+class Solution {
     public int numSquares(int n) {
+        if(n <= 0) return n;
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for(int i = 1; i <= n; ++i) {
+        for(int i = 1; i < dp.length; i++){
             int min = Integer.MAX_VALUE;
-            int j = 1;
-            while(i - j*j >= 0) {
-                min = Math.min(min, dp[i - j*j] + 1);
-                ++j;
+            for(int j = 1; j <= (i / j); j++){
+                min = Math.min(min, dp[i - j * j] + 1);
             }
             dp[i] = min;
-        }		
+        }
         return dp[n];
     }
 }
