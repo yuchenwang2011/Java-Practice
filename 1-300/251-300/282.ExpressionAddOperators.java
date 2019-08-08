1d78682cd0add1c87d2810324b2077acedfa11a5
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-08 17:24:54 -0400

    Update 282.ExpressionAddOperators.java

--- 1-300/251-300/282.ExpressionAddOperators.java
+++ 1-300/251-300/282.ExpressionAddOperators.java
@@ -56,6 +56,10 @@ class Solution {
                 
                 helper(rst, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
                 
+                //for example, if you have a sequence of 12345 and you have proceeded to 1 + 2 + 3, 
+                //now your eval is 6 right? If you want to add a * between 3 and 4, you would take 3 as the digit to be multiplied,
+                //so you want to take it out from the existing eval. You have 1 + 2 + 3 * 4 
+                //and the eval now is (1 + 2 + 3) - 3 + (3 * 4).
                 helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
             }
         }
