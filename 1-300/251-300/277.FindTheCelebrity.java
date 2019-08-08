fdd8e2e898370d5ff56b430013f31bc4cf98b4b6
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-08 14:38:07 -0400

    Update 277.FindTheCelebrity.java

--- 1-300/251-300/277.FindTheCelebrity.java
+++ 1-300/251-300/277.FindTheCelebrity.java
@@ -15,32 +15,18 @@ Note: There will be exactly one celebrity if he/she is in the party.
 Return the celebrity's label if there is a celebrity in the party. 
 If there is no celebrity, return -1.
 
-Answer: 
-/* The knows API is defined in the parent class Relation.
-      boolean knows(int a, int b); */
-
+Answer:
 public class Solution extends Relation {
     public int findCelebrity(int n) {
-        //Inspired by this solution
-        //https://leetcode.com/discuss/56413/java-solution-two-pass
-        int candidate = -1;
-        if( n <= 0) return candidate;
-        candidate = 0;  //assume the first person is the celebrity 
-        for(int i = 1; i <= n-1; i++){
-            if(knows(candidate,i)) candidate =i; //if cele knows i, it means it's fake
-            //and also people knows i, so i can be a candidate
-            //if i don't know any people after i, it means, i is known by last fake cele, and don't know people behind him
-            //that means i is a possible candidate
+        if(n <= 0) return -1;
+        int candidate = 0;
+        for(int i = 1; i < n; i++){
+            if(knows(candidate, i)) candidate = i;
         }
-        
-        //next round is to double check if the candidate is real
-        for(int i = 0; i <= n-1; i++){
-            //note, in the if statement, must use return, you can't set candidate = -1. because loop not finished yet
-            //candidate can't know others and everyone must know candidate
-            if(i < candidate && (knows(candidate,i) == true || knows(i,candidate) == false)) return -1;
-            if(i > candidate && !knows(i,candidate)) return -1; //knows(cele,i) is checked in last round,when i>cele
+        for(int i = 0; i < n; i++){
+            if(i == candidate) continue;
+            if(!knows(i, candidate) || knows(candidate, i)) return -1;
         }
-        
         return candidate;
     }
 }
