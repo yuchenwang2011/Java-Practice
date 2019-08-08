7dea348e662680e9c8486053dbf7bbcfe6a0a6da
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-08 16:34:51 -0400

    Update 281.ZigzagIterator.java

--- 1-300/251-300/281.ZigzagIterator.java
+++ 1-300/251-300/281.ZigzagIterator.java
@@ -24,3 +24,29 @@ Input:
 Output: [1,4,8,2,5,9,3,6,7].
 
 Answer:
+public class ZigzagIterator {
+    Queue<Iterator> queue;
+    
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+        queue = new LinkedList<>();
+        if(!v1.isEmpty()) queue.offer(v1.iterator());
+        if(!v2.isEmpty()) queue.offer(v2.iterator());
+    }
+
+    public int next() {
+        Iterator<Integer> iter = queue.poll();
+        int next = iter.next();
+        if(iter.hasNext()) queue.offer(iter);
+        return next;
+    }
+
+    public boolean hasNext() {
+        return !queue.isEmpty();
+    }
+}
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i = new ZigzagIterator(v1, v2);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
