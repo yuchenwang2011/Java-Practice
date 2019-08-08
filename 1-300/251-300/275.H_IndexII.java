2a451097452ce9885f1a974cc5d2e11c77cab93b
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-08 12:59:52 -0400

    Update 275.H_IndexII.java

--- 1-300/251-300/275.H_IndexII.java
+++ 1-300/251-300/275.H_IndexII.java
@@ -7,18 +7,21 @@ Hint:
 Expected runtime complexity is in O(log n) and the input is sorted.
 
 Answer:
-public class Solution {
+//这题和274的第一个慢的解法一样，先研究研究那个
+class Solution {
     public int hIndex(int[] citations) {
         if(citations == null || citations.length == 0) return 0;
-        int start = 0, end = citations.length - 1;
+        int start = 0;
+        int end = citations.length - 1;
+        int n = citations.length;
         while(start + 1 < end){
             int mid = start + (end - start) / 2;
-            if(citations[mid] == citations.length - mid) return citations.length - mid;
-            else if (citations[mid] > citations.length - mid) end = mid;
-            else start = mid;
+            if(citations[mid] == n - mid) return n - mid;
+            else if (citations[mid] < n - mid) start = mid;
+            else end = mid;
         }
-        if(citations[start] >= citations.length - start) return citations.length - start;
-        if(citations[end] >= citations.length - end) return citations.length - end;
+        if(citations[start] >= n - start) return n - start;
+        if(citations[end] >= n - end) return n - end;
         return 0;
     }
 }
