f835f351d662cbffd367d98b56e1becaf7a14ecc
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-08 23:57:08 -0400

    Update 285.InorderSuccessorInBST.java

--- 1-300/251-300/285.InorderSuccessorInBST.java
+++ 1-300/251-300/285.InorderSuccessorInBST.java
@@ -33,3 +33,16 @@ public class Solution {
         return inorderSuccessor(root.right,p);
     }
 }
+
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if(root == null || p == null) return root;
+        if(root.val <= p.val) {
+            return inorderSuccessor(root.right, p);
+        } else {
+            TreeNode left = inorderSuccessor(root.left, p);
+            return left == null ? root : left;
+        }
+    }
+}
+
