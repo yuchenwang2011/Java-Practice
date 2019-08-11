0a4a3c6c71296a2c31ae170fef768fe32b2e3116
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-11 19:22:12 -0400

    Update 295.FindMedianFromDataStream.java

--- 1-300/251-300/295.FindMedianFromDataStream.java
+++ 1-300/251-300/295.FindMedianFromDataStream.java
@@ -28,16 +28,27 @@ Accepted 120,618 Submissions 318,528
 
 Answer:
 class MedianFinder {
-
+    PriorityQueue<Long> small;
+    PriorityQueue<Long> big;
+    
     public MedianFinder() {
-        
+        small = new PriorityQueue<>();
+        big = new PriorityQueue<>();
     }
     
+    //这里的目的是争取让small和big平分所有的数，并且保持big总是多点
+    //这样如果是奇数，big就是第一个。如果是偶数，就是big和small的第一个
+    //想一个x y坐标轴，big就是个向上的抛物线。small就是个向下的抛物线
+    //small的第一个，其实就是big的前一个
     public void addNum(int num) {
-        
-    }
+        big.offer((long) num);
+        small.offer(-big.poll());
+        if(big.size() < small.size()) big.offer(-small.poll());
+    } 
     
     public double findMedian() {
-        
+        if(big.size() > small.size()) return big.peek();
+        //注意这里是peek,不是poll，因为可能call这个find method好几次
+        return (big.peek() + (-small.peek())) / 2.0;
     }
 }
