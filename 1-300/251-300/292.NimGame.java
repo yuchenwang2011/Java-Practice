36adf9a1eb0719ea4f195f42af48bc9748fc9429
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-11 16:38:06 -0400

    Update 292.NimGame.java

--- 1-300/251-300/292.NimGame.java
+++ 1-300/251-300/292.NimGame.java
@@ -18,7 +18,7 @@ If there are 5 stones in the heap,
 could you figure out a way to remove the stones such that you will always be the winner?
 
 Answer:
-public class Solution {
+class Solution {
     //俩小孩拿石头，第一个小孩可以拿走四的余数，剩下的时候不管对方拿多少，就拿可以凑成4的数，这样每次凑四
     //这样就可以保证自己拿走最后的一把。所以对于是个4的倍数来说，第一个小孩不管怎么拿，第二个小孩只要拿走相应的凑四的话
     //这样第二个小孩可以保证自己肯定赢
@@ -26,24 +26,4 @@ public class Solution {
         if(n <= 0 ) return false;
         return n % 4 !=0;
     }
-    /*
-    public boolean canWinNim(int n) {
-        HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
-        System.out.println(Integer.MAX_VALUE);
-        return helper(n, map);
-    }
-    
-    public boolean helper(int n, HashMap<Integer, Boolean> map){
-         if(n<=3) return true;
-         if(map.containsKey(n)) return map.get(n);
-         for(int i = 1; i <=3; i++){
-             if(!helper(n-i, map)) {
-                 map.put(n-i,true);
-                 return true;
-             }
-         }
-         map.put(n, false);
-         return false;
-    }
-    */
 }
