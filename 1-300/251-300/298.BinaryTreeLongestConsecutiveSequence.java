c86479218cbbc3d59e512bb7eab99b4d81883e83
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-12 22:42:34 -0400

    Update 298.BinaryTreeLongestConsecutiveSequence.java

--- 1-300/251-300/298.BinaryTreeLongestConsecutiveSequence.java
+++ 1-300/251-300/298.BinaryTreeLongestConsecutiveSequence.java
@@ -2,7 +2,8 @@
 Total Accepted: 5989 Total Submissions: 16664 Difficulty: Medium
 Given a binary tree, find the length of the longest consecutive sequence path.
 
-The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The longest consecutive path need to be from parent to child (cannot be the reverse).
+The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. 
+   The longest consecutive path need to be from parent to child (cannot be the reverse).
 
 For example,
    1
@@ -23,31 +24,19 @@ Longest consecutive sequence path is 3-4-5, so return 3.
 Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
 
 Answer:
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-//At first My answer was similar to first link, then I removed the global variable as the second link
-//https://leetcode.com/discuss/66584/easy-java-dfs-is-there-better-time-complexity-solution
-//https://leetcode.com/discuss/68723/simple-recursive-dfs-without-global-variable
-public class Solution {
+class Solution {
     public int longestConsecutive(TreeNode root) {
-        int count = 0;
-        if(root == null) return count;
-        return dfs(root, null, count);
+        if(root == null) return 0;
+        return helper(root, null, 0);
     }
     
-    public int dfs(TreeNode root, TreeNode prev, int count){
+    public int helper(TreeNode root, TreeNode parent, int count){
         if(root == null) return 0;
-        if (prev == null) count ++;
-        else count = (root.val - 1 == prev.val) ? count + 1 : 1;
-        int left = dfs(root.left, root, count);
-        int right = dfs(root.right, root, count);
+        if(parent != null && root.val == parent.val + 1) count++;
+        else count = 1;
+        
+        int left = helper(root.left, root, count);
+        int right = helper(root.right, root, count);
         return Math.max(count, Math.max(left, right));
     }
 }
