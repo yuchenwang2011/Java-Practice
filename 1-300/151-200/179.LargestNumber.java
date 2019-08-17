6d7618219dd55621f2637c7503fd401377b81934
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-17 00:06:56 -0400

    Update 179.LargestNumber.java

--- 1-300/151-200/179.LargestNumber.java
+++ 1-300/151-200/179.LargestNumber.java
@@ -7,6 +7,8 @@ For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
 Note: The result may be very large, so you need to return a string instead of an integer.
 
 Answer:
+//就这么记，想正常升序排序就a.compareTo(b)。但凡是一个题稍微要懂点脑子的题，
+//把变形的、打乱了两个形参顺序的b放到前面，你就需要b.compareTo(a)
 class Solution {
     public String largestNumber(int[] nums) {
         if(nums == null || nums.length == 0) return "0";
