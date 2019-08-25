c5130db58f54cfe9c4c0aecfe7325bdabd40d668
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-08-24 20:44:12 -0400

    Update 249.GroupShiftedStrings.java

--- 1-300/201-250/249.GroupShiftedStrings.java
+++ 1-300/201-250/249.GroupShiftedStrings.java
@@ -19,25 +19,19 @@ Note: For the return value, each inner list's elements must follow the lexicogra
 
 Answer:
 public class Solution {
-    //*************Remember the API for HashMap!!!!! And Collections.sort(list)
     public List<List<String>> groupStrings(String[] strings) {
         List<List<String>> result = new ArrayList<List<String>>();
         if(strings == null || strings.length == 0) return result;
-        //creat a hashmap to save a string key and a list of strings belongs to a single category
         HashMap<String, List<String>> map = new HashMap<String, List<String>>();
         
-        //Now generate the key: make all bcd, efg back to the beginning of this category abc
-        //if they can back to key abc, means they can be added to the same list in hashmap
-        for(String str : strings ){
+        for(String str : strings){
             String key = generateKey(str);
-            //if this key category doesn't exist, create one list first, then add str into it
             if(map.containsKey(key) == false) {
                 map.put(key,new ArrayList<String>());
             }
             map.get(key).add(str);
         }
-        //Now to collect results from the HashMap, first keySet gives all the keys
-        //second use the key to get the corresponding set of Strings
+
         for(String key : map.keySet()){
             List<String> list = map.get(key);
             Collections.sort(list);
