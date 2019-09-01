ba7ff217ac393aceb6fed3acdd501eecb085ea78
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-09-01 15:27:31 -0400

    Update 3.LongestSubstringWithoutRepeatingCharacters.java

--- 1-300/1-50/3.LongestSubstringWithoutRepeatingCharacters.java
+++ 1-300/1-50/3.LongestSubstringWithoutRepeatingCharacters.java
@@ -5,29 +5,6 @@ For example, the longest substring without repeating letters for "abcabcbb" is "
 For "bbbbb" the longest substring is "b", with the length of 1.
 
 Answer:
-import java.util.*;
-public class Solution {
-    //*****charAt is ()!!!!!!
-    //I got inspired by this answer: https://leetcode.com/discuss/27201/share-my-java-solution
-    public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0) {
-            return 0;
-        }
-        int start =0; int end = 0; int maxLength = 0;
-        HashMap<Character, Integer> myHash = new HashMap<Character,Integer>();
-        for(;end < s.length();end++) {
-            if(myHash.containsKey(s.charAt(end))){
-                //this is for example baacdebxp, the second b gives the location before the start, which is the second a now  
-               start = Math.max(start, myHash.get(s.charAt(end))+1);
-            }
-            myHash.put(s.charAt(end),end);
-            maxLength = Math.max(maxLength, end-start+1);
-        }
-        return maxLength;
-    }
-}
-
-//this is my second round answer
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
         if(s == null || s.length() == 0) return 0;
