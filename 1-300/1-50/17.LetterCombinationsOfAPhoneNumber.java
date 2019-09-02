ae2dbfec782529cee64582d84787704206e21240
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-09-02 17:53:05 -0400

    Update 17.LetterCombinationsOfAPhoneNumber.java

--- 1-300/1-50/17.LetterCombinationsOfAPhoneNumber.java
+++ 1-300/1-50/17.LetterCombinationsOfAPhoneNumber.java
@@ -10,6 +10,10 @@ Although the above answer is in lexicographical order, your answer could be in a
 
 Answer:
 //DFS + BFS两种解法
+//O(4^n) For DFS/ BFS, you can typically calculate the height of the calculation tree, 
+// and then multiply the time complexity of each level of the calculation tree. 
+// For this problem, if you press one button, you have 4 choice, if you press N times, 
+// you have to multiply N time to build the new String.
 class Solution {
     private static final String[] NUMS = new String[]{"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public List<String> letterCombinations(String digits) {
