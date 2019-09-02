1156dd4a2508213c56f721e5e784df462645beb5
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-09-02 14:15:16 -0400

    Update 15.3Sum.java

--- 1-300/1-50/15.3Sum.java
+++ 1-300/1-50/15.3Sum.java
@@ -15,43 +15,6 @@ The solution set must not contain duplicate triplets.
 Answer:
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        //****Remember the syntax for List<List<Integer>> whole line
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        if(nums == null || nums.length < 3) {
-            return result;
-        }
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length-2 ; i++){
-            int start = i+1, end = nums.length-1;
-            
-            if(i>0 && nums[i] == nums[i-1]){
-                continue;
-            }
-            
-            while(start < end){
-                if(start < end && nums[i] + nums[start] + nums[end] == 0){
-                    while(start < end && nums[start] == nums[start+1]){
-                      start++;
-                    }
-                    while(start < end && nums[end] == nums[end-1]) {
-                      end--;
-                    }
-                    result.add(Arrays.asList(nums[i],nums[start],nums[end]));
-                    start++;end--;
-                } else if (start < end && nums[i] + nums[start] + nums[end] > 0){
-                    end--;
-                } else if (start < end && nums[i]+nums[start]+nums[end] <0) {
-                    start++;
-                }
-            }
-        }
-        return result;
-    }
-}
-
-//This is my answer second round
-public class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         if(nums == null || nums.length < 3) return result;
         Arrays.sort(nums);
