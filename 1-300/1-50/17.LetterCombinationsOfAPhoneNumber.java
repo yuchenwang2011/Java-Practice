76bd5d7cda95b6a4b08a77d30802d08d3b6a5c58
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-09-02 18:07:55 -0400

    Update 17.LetterCombinationsOfAPhoneNumber.java

--- 1-300/1-50/17.LetterCombinationsOfAPhoneNumber.java
+++ 1-300/1-50/17.LetterCombinationsOfAPhoneNumber.java
@@ -37,19 +37,18 @@ class Solution {
 
 
 class Solution {
-    public List<String> letterCombinations(String digits) {
-		LinkedList<String> ans = new LinkedList<String>();
-		if(digits.isEmpty()) return ans;
-		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-		ans.add("");
-		for(int i =0; i<digits.length();i++){
-			int x = Character.getNumericValue(digits.charAt(i));
-			while(ans.peek().length()==i){
-				String t = ans.remove();
-				for(char s : mapping[x].toCharArray())
-					ans.add(t+s);
-			}
-		}
-		return ans;
+  public List<String> letterCombinations(String digits) {
+    LinkedList<String> ans = new LinkedList<String>();
+    if(digits.isEmpty()) return ans;
+    String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    ans.add("");
+    while(ans.peek().length()!=digits.length()){
+        String remove = ans.remove();
+        String map = mapping[digits.charAt(remove.length())-'0'];
+	for(char c: map.toCharArray()){
+	    ans.addLast(remove+c);
 	}
+    }
+    return ans;
+  }
 }
