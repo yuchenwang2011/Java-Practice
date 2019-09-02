87e401a25122078e3188eea914f8d765a624d320
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-09-02 13:32:58 -0400

    Update 13.RomanToInteger.java

--- 1-300/1-50/13.RomanToInteger.java
+++ 1-300/1-50/13.RomanToInteger.java
@@ -50,3 +50,37 @@ https://leetcode.com/problems/roman-to-integer/discuss/6529
         return (int) result;        
     }
 }
+
+
+class Solution {
+    public  int romanToInt(String s) {
+        int res = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            switch (c) {
+            case 'I':
+                res += (res >= 5 ? -1 : 1);
+                break;
+            case 'V':
+                res += 5;
+                break;
+            case 'X':
+                res += 10 * (res >= 50 ? -1 : 1);
+                break;
+            case 'L':
+                res += 50;
+                break;
+            case 'C':
+                res += 100 * (res >= 500 ? -1 : 1);
+                break;
+            case 'D':
+                res += 500;
+                break;
+            case 'M':
+                res += 1000;
+                break;
+            }
+        }
+        return res;
+    }
+}
