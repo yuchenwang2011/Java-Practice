5c3706c7b6eecc15202240e1f7173c912dc4c140
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-09-02 17:25:26 -0400

    Update 17.LetterCombinationsOfAPhoneNumber.java

--- 1-300/1-50/17.LetterCombinationsOfAPhoneNumber.java
+++ 1-300/1-50/17.LetterCombinationsOfAPhoneNumber.java
@@ -9,49 +9,6 @@ Note:
 Although the above answer is in lexicographical order, your answer could be in any order you want.
 
 Answer:
-//********Remember: must use break when using switch!!
-public class Solution {
-    public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<String>();
-        if(digits == null || digits.length() ==0) return result;
-        process(digits,result,0,"");
-        return result;
-    }
-    
-    public void process(String digits, List<String> result, int idx, String tmp){
-        if(idx >= digits.length()) {
-            result.add(tmp);
-            return;
-        }
-        String s = convert(digits.charAt(idx));
-        for(int j = 0; j < s.length(); j++) {
-            process(digits,result,idx+1,tmp + s.charAt(j));
-        }
-    }
-    
-    public String convert(char c){
-        int val = c - '0';
-        String result = "";
-        if(c == '*' || c == '#') return "";
-        switch(val){
-            case 1: case 0: result = "";break;
-            case 2: result = "abc"; break;
-            case 3: result = "def"; break;
-            case 4: result ="ghi";  break;
-            case 5: result = "jkl"; break;
-            case 6: result = "mno"; break;
-            case 7: result = "pqrs";break;
-            case 8: result = "tuv"; break;
-            case 9: result = "wxyz";break;
-            default: break;
-        }
-        return result;
-    }
-}
-
-//got inspired by Roma number to integeter question
-=======================My second round two years later=================================
-Solution:
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
