4ebcb8c438acf0678e9c5cd3e1c625a8a28f3098
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-09-01 21:04:27 -0400

    Update 9.PalindromeNumber.java

--- 1-300/1-50/9.PalindromeNumber.java
+++ 1-300/1-50/9.PalindromeNumber.java
@@ -5,14 +5,11 @@ Determine whether an integer is a palindrome. Do this without extra space.
 click to show spoilers.
 
 Some hints:
-Could negative integers be palindromes? (ie, -1)
-
-If you are thinking of converting the integer to string, note the restriction of using extra space.
-
-You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", 
-you know that the reversed integer might overflow. How would you handle such case?
-
-There is a more generic way of solving this problem.
+1. Could negative integers be palindromes? (ie, -1)
+2. If you are thinking of converting the integer to string, note the restriction of using extra space.
+3.You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", 
+4.you know that the reversed integer might overflow. How would you handle such case?
+5.There is a more generic way of solving this problem.
 
 Answer:
 //两个解法
@@ -24,7 +21,6 @@ public class Solution {
         while(x != 0){
             y = y * 10 + x % 10;
             x = x / 10;
-            System.out.println(y);
         }
         return backup == y;
     }
