62c65ea59e47118694d3dcec74327bb9dab1f6e8
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-09-13 23:15:28 -0400

    Update 60.PermutationSequence.java

--- 1-300/51-100/60.PermutationSequence.java
+++ 1-300/51-100/60.PermutationSequence.java
@@ -1,9 +1,6 @@
-60. Permutation Sequence My Submissions QuestionEditorial Solution
-Total Accepted: 52125 Total Submissions: 208715 Difficulty: Medium
-The set [1,2,3,…,n] contains a total of n! unique permutations.
-
-By listing and labeling all of the permutations in order,
-We get the following sequence (ie, for n = 3):
+60. Permutation Sequence . Medium
+The set [1,2,3,...,n] contains a total of n! unique permutations.
+By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
 
 "123"
 "132"
@@ -11,9 +8,20 @@ We get the following sequence (ie, for n = 3):
 "231"
 "312"
 "321"
+    
 Given n and k, return the kth permutation sequence.
 
-Note: Given n will be between 1 and 9 inclusive.
+Note:
+Given n will be between 1 and 9 inclusive.
+Given k will be between 1 and n! inclusive.
+
+Example 1:
+Input: n = 3, k = 3
+Output: "213"
+    
+Example 2:
+Input: n = 4, k = 9
+Output: "2314"
 
 Answer:
 class Solution {
