97c8f5c55b875d3694a8c316390bf2c5a98a9026
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-09-13 22:54:05 -0400

    Update 58.LengthOfLastWord.java

--- 1-300/51-100/58.LengthOfLastWord.java
+++ 1-300/51-100/58.LengthOfLastWord.java
@@ -1,15 +1,13 @@
-
-f Last Word My Submissions Question
-Total Accepted: 86786 Total Submissions: 299441 Difficulty: Easy
-Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+58. Length of Last Word . Easy
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
+return the length of last word in the string.
 
 If the last word does not exist, return 0.
-
 Note: A word is defined as a character sequence consists of non-space characters only.
 
-For example, 
-Given s = "Hello World",
-return 5.
+Example:
+Input: "Hello World"
+Output: 5
 
 Answer:
 //First one is my own answer, second one is most other people solve it
