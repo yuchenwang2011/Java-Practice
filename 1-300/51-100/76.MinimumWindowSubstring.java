cc111c8a4aef120b45ed9426b06bfadfb00ad89b
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-09-19 01:58:26 -0400

    Update 76.MinimumWindowSubstring.java

--- 1-300/51-100/76.MinimumWindowSubstring.java
+++ 1-300/51-100/76.MinimumWindowSubstring.java
@@ -36,128 +36,36 @@ Answer:
 //And use counter for the number of chars of t to be found in s. The key part is m[s[end]]--;. 
 //We decrease count for each char in s. If it does not exist in t, the count will be negative.
 
-
-=====================My digested version======================
-    public String minWindow(String s, String t) {
-        int[] map = new int[128];
-        for(char c : t.toCharArray()){
-            map[c]++;
-        }
-
-        int left = 0;
-        int right = 0;
-        int resultStart = 0;
-        int resultLength = Integer.MAX_VALUE;
-        int count = t.length();
-
-        while(right < s.length()){
-            if(map[s.charAt(right++)]-- > 0){
-                count--;
-            }
-            while(count == 0){
-                if(right - left < resultLength){
-                    resultLength = right - left;
-                    resultStart = left;
-                }
-                if(map[s.charAt(left++)]++ == 0){
-                    count++;
-                }
-            }
-        }
-        return resultLength == Integer.MAX_VALUE ? "" : s.substring(resultStart, resultStart + resultLength);
-    }
-
-
-==================================================================
-
-
 class Solution {
     public String minWindow(String s, String t) {
-        int[] map = new int[128];
-        int res = Integer.MAX_VALUE, left = 0, right = 0, begin = 0, counter = t.length();
-        for (int i = 0; i < t.length(); i++) {
-            map[t.charAt(i) - 'A']++;
+        String result = "";
+        if(s == null || s.length() == 0 || t == null || t.length() == 0) return result;
+        
+        int[] map = new int[256];
+        //注意这里是t
+        for(int i = 0; i < t.length(); i++){
+            map[t.charAt(i)]++;
         }
-        while (right < s.length()) {
-            if (map[s.charAt(right++) - 'A']-- > 0) counter--;  //这个数应该出现的次数减一
-            while (counter == 0) {  //缩短左边界
-                if (right - left < res) {
-                    res = right - left;
-                    begin = left;
+        
+        int start = 0;
+        int end = 0;
+        int count = t.length();
+        int max = Integer.MAX_VALUE;
+        
+        while(end < s.length()){
+            if(map[s.charAt(end++)]-- > 0) {
+                count--;
+            }
+            while(count == 0) {
+                if(end - start < max) {
+                    max = end - start;
+                    result = s.substring(start, end);
                 }
-                if (map[s.charAt(left++) - 'A']++ == 0) counter++;
+                if(map[s.charAt(start++)]++ == 0) {
+                    count++;
+                }
             }
         }
-        return res == Integer.MAX_VALUE ? "" : s.substring(begin, begin + res);
+        return result;
     }
 }
-
- public String minWindow(String s, String t) {
-        int[] map = new int[128];
-        
-        for (char c : t.toCharArray()) map[c]++;
-        
-        int count = t.length(), begin = 0, end = 0, d = Integer.MAX_VALUE, head = 0;
-        
-        while (end < s.length()) {
-            if (map[s.charAt(end++)]-- > 0) count--;
-            while (count == 0) {
-                if (end - begin < d) {
-                    d = end - begin;
-                    head = begin;
-                }
-                if (map[s.charAt(begin++)]++ == 0) 
-                    count++;
-            }
-        }
-        
-        return d == Integer.MAX_VALUE ? "" : s.substring(head, head+d);
-    }
-    
-    
-      public String minWindow(String s, String t) {
-    int [] map = new int[128];
-    for (char c : t.toCharArray()) {
-      map[c]++;
-    }
-    int start = 0, end = 0, minStart = 0, minLen = Integer.MAX_VALUE, counter = t.length();
-    while (end < s.length()) {
-      final char c1 = s.charAt(end);
-      if (map[c1] > 0) counter--;
-      map[c1]--;
-      end++;
-      while (counter == 0) {
-        if (minLen > end - start) {
-          minLen = end - start;
-          minStart = start;
-        }
-        final char c2 = s.charAt(start);
-        map[c2]++;
-        if (map[c2] > 0) counter++;
-        start++;
-      }
-    }
-
-    return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
-  }
-  
-  
-  
-      public String minWindow(String s, String t) {
-        int[] cnt = new int[256];
-        for (char c : t.toCharArray()) cnt[c]++;
-        
-        int min = Integer.MAX_VALUE, from = 0, total = t.length();
-        for (int i = 0, j = 0; i < s.length(); i++) {
-            if (cnt[s.charAt(i)]-- > 0) total--;
-            while (total == 0) {                    // total=0 means valid window
-                if (i - j + 1 < min) {
-                    min = i - j + 1;
-                    from = j;
-                }
-                if (++cnt[s.charAt(j++)] > 0) total++;
-            }
-        }
-        return (min == Integer.MAX_VALUE) ? "" : s.substring(from, from + min);
-    }
-  
