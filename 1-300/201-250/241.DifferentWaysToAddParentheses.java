ebbe68401f50756ff4f81876b91211c4a6d7ccd5
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-09-30 22:25:36 -0400

    Update 241.DifferentWaysToAddParentheses.java

--- 1-300/201-250/241.DifferentWaysToAddParentheses.java
+++ 1-300/201-250/241.DifferentWaysToAddParentheses.java
@@ -3,18 +3,14 @@ Total Accepted: 19839 Total Submissions: 57754 Difficulty: Medium
 Given a string of numbers and operators, return all possible results 
 from computing all the different possible ways to group numbers and operators. The valid operators are +, - and *.
 
-
 Example 1
 Input: "2-1-1".
-
 ((2-1)-1) = 0
 (2-(1-1)) = 2
 Output: [0, 2]
 
-
 Example 2
 Input: "2*3-4*5"
-
 (2*(3-(4*5))) = -34
 ((2*3)-(4*5)) = -14
 ((2*(3-4))*5) = -10
