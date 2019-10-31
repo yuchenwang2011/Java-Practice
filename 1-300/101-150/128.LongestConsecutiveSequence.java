20a71f01c6f67dafca2a1bd98e8ffb620c76ee7d
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-10-31 19:18:56 -0400

    Update 128.LongestConsecutiveSequence.java

--- 1-300/101-150/128.LongestConsecutiveSequence.java
+++ 1-300/101-150/128.LongestConsecutiveSequence.java
@@ -11,6 +11,7 @@ Output: 4
 Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
 
 Answer:
+//必须掌握两种解法
 class Solution {
     public int longestConsecutive(int[] nums) {
         int result = 0;
@@ -33,5 +34,63 @@ class Solution {
             result = Math.max(result, upper - lower - 1);
         }
         return result;
+    }
+}
+
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        UnionFind uf = new UnionFind(nums.length);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])) continue;
+            map.put(nums[i], i);
+            
+            if(map.containsKey(nums[i] + 1)) {
+                uf.union(i, map.get(nums[i] + 1));
+            }
+            if(map.containsKey(nums[i] - 1)) {
+                uf.union(i, map.get(nums[i] - 1));
+            }
+        }
+        return uf.maxCount();
+    }
+    
+    public class UnionFind{
+        int[] parent;
+        //这里的union find是用数组的index来建立的，union和find的都是index
+        public UnionFind(int n){
+            parent = new int[n];
+            for(int i = 0; i < n; i++){
+                parent[i] = i;
+            }
+        }
+        
+        public int find(int id){
+            if(id == parent[id]) return id;
+            parent[id] = find(parent[id]);
+            return parent[id];
+        }
+        
+        public void union(int id1, int id2){
+            int root1 = find(id1);
+            int root2 = find(id2);
+            if(root1 != root2) {
+                parent[root1] = root2;
+            }
+        }
+        
+        public int maxCount(){
+            int max = 0;
+            int[] count = new int[parent.length];
+            for(int i = 0; i < parent.length; i++){
+                //这里就是找出带头大哥的出现次数
+                //每个小弟都挨个问一遍，你大哥是谁，然后把大哥票数加1,最后看哪个大哥票数最多
+                count[find(i)]++;
+                max = Math.max(max, count[find(i)]);
+            }
+            return max;
+        }
     }
 }
