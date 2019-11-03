a6b41d7f8b3fe9cc99c91b36868d32cf2dfe47cb
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-11-03 00:58:20 -0400

    Update 347.TopKFrequentElements.java

--- 301-600/301-350/347.TopKFrequentElements.java
+++ 301-600/301-350/347.TopKFrequentElements.java
@@ -18,3 +18,32 @@ Your algorithm's time complexity must be better than O(n log n), where n is the 
 Accepted 264,695 Submissions 462,003
 
 Answer:
+class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        if(nums == null || nums.length == 0 || k <= 0 || k > nums.length) return result;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
+        for(int key : map.keySet()){
+            int freq = map.get(key);
+            if(bucket[freq] == null) bucket[freq] = new ArrayList<>();
+            bucket[freq].add(key);
+        }
+        
+        int i = bucket.length - 1;
+        while(k > 0){
+            while(i >= 0 && bucket[i] == null) {
+                i--;
+            }
+            result.addAll(bucket[i]);
+            k-= bucket[i].size();
+            i--;
+        }
+        
+        return result;
+    }
+}
