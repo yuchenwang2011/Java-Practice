356afb7b6f360e533d9d86a1a2d08f8455871252
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-11-08 23:36:52 -0500

    Update 304.RangeSumQuery2D_Immutable.java

--- 301-600/301-350/304.RangeSumQuery2D_Immutable.java
+++ 301-600/301-350/304.RangeSumQuery2D_Immutable.java
@@ -27,31 +27,50 @@ There are many calls to sumRegion function.
 You may assume that row1 ≤ row2 and col1 ≤ col2.
 
 Answer:
-public class NumMatrix {
-    //Got inspired by this answer
-    //http://www.cnblogs.com/grandyang/p/4958789.html
-    //*********Remember this question, how to use 2D dp matrix
-    
+To calculate sums, the ideas as below
+
++-----+-+-------+     +--------+-----+     +-----+---------+     +-----+--------+
+|     | |       |     |        |     |     |     |         |     |     |        |
+|     | |       |     |        |     |     |     |         |     |     |        |
++-----+-+       |     +--------+     |     |     |         |     +-----+        |
+|     | |       |  =  |              |  +  |     |         |  -  |              |
++-----+-+       |     |              |     +-----+         |     |              |
+|               |     |              |     |               |     |              |
+|               |     |              |     |               |     |              |
++---------------+     +--------------+     +---------------+     +--------------+
+
+   sums[i][j]      =    sums[i-1][j]    +     sums[i][j-1]    -   sums[i-1][j-1]   +  
+
+                        matrix[i-1][j-1]
+So, we use the same idea to find the specific area's sum.
+
++---------------+   +--------------+   +---------------+   +--------------+   +--------------+
+|               |   |         |    |   |   |           |   |         |    |   |   |          |
+|   (r1,c1)     |   |         |    |   |   |           |   |         |    |   |   |          |
+|   +------+    |   |         |    |   |   |           |   +---------+    |   +---+          |
+|   |      |    | = |         |    | - |   |           | - |      (r1,c2) | + |   (r1,c1)    |
+|   |      |    |   |         |    |   |   |           |   |              |   |              |
+|   +------+    |   +---------+    |   +---+           |   |              |   |              |
+|        (r2,c2)|   |       (r2,c2)|   |   (r2,c1)     |   |              |   |              |
++---------------+   +--------------+   +---------------+   +--------------+   +--------------+
+
+
+class NumMatrix {
     private int[][] dp;
-    
     public NumMatrix(int[][] matrix) {
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
-        int m = matrix.length, n = matrix[0].length;
-        dp = new int[m+1][n+1];
-        for(int i = 1; i < m + 1; i++){
-            for(int j = 1; j < n + 1; j++){
-                dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + matrix[i-1][j-1];
+        dp = new int[matrix.length + 1][matrix[0].length + 1];
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[0].length; j++){
+                //每个元素=左边的       +  上面的      -  重合的           + num
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
         }
     }
-
+    
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return dp[row2 + 1][col2 + 1] - dp[row2 + 1][col1] - dp[row1][col2 + 1] + dp[row1][col1]; 
+        if(row1 < 0 || row2 < 0 || col1 > dp[0].length - 1 || col2 > dp[0].length - 1) return 0;
+        if(row1 > row2 || col1 > col2) return 0;
+        return dp[row2 + 1][col2 + 1] - dp[row2 + 1][col1] - dp[row1][col2 + 1] + dp[row1][col1];
     }
 }
-
-
-// Your NumMatrix object will be instantiated and called as such:
-// NumMatrix numMatrix = new NumMatrix(matrix);
-// numMatrix.sumRegion(0, 1, 2, 3);
-// numMatrix.sumRegion(1, 2, 3, 4);
