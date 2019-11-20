265ea50fd4d3c24316927cb69153f03402a7e3ab
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-11-19 21:08:08 -0500

    Update 2.AddTwoNumbers.java

--- 1-300/1-50/2.AddTwoNumbers.java
+++ 1-300/1-50/2.AddTwoNumbers.java
@@ -8,28 +8,26 @@ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 
 Answer:
-public class Solution {
+class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 == null) return null;
-        ListNode newHead = new ListNode(-1);
-        ListNode tmp = newHead;
-        int sum = 0;
-        while(l1 != null || l2 != null) {
-            sum = sum/10;
-            if(l1 != null){
-                sum = sum + l1.val;
-                l1 = l1.next;
-            }
-            if(l2 != null){
-                sum = sum + l2.val;
-                l2 = l2.next;
-            }
-            tmp.next = new ListNode(sum%10);
-            tmp = tmp.next;
-        } 
-        if(sum/10 == 1){
-            tmp.next = new ListNode(1);
+        if(l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
         }
-        return newHead.next;
+        
+        int carry = 0;
+        ListNode prev = new ListNode(-1);
+        ListNode l3 = prev;
+        while(l1 != null || l2 != null){
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int sum = val1 + val2 + carry;
+            prev.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            prev = prev.next;
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        if(carry != 0) prev.next = new ListNode(carry);
+        return l3.next;
     }
 }
