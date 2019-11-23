e2c2dd5864cc3bec651deec759f37fad22b7c6dc
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-11-22 23:16:49 -0500

    Update 319.BulbSwitcher.java

--- 301-600/301-350/319.BulbSwitcher.java
+++ 301-600/301-350/319.BulbSwitcher.java
@@ -24,3 +24,9 @@ public class Solution {
         return (int) Math.sqrt(n);
     }
 }
+
+
+
+Divisors come in pairs, like i=12 has divisors 1 and 12, 2 and 6, and 3 and 4. 
+    Except when i is a square, like 36 has divisors 1 and 36, 2 and 18, 3 and 12, 4 and 9, 
+and double divisor 6. So bulb i ends up on if and only if i is a square.
