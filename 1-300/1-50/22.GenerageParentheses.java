1d0ae477dea55db5aa0f49b68975264b99a474e2
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-11-23 18:03:45 -0500

    Update 22.GenerageParentheses.java

--- 1-300/1-50/22.GenerageParentheses.java
+++ 1-300/1-50/22.GenerageParentheses.java
@@ -7,47 +7,21 @@ For example, given n = 3, a solution set is:
 "((()))", "(()())", "(())()", "()(())", "()()()"
 
 Answer:
-public class Solution {
-    public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<String>();
-        if(n == 0) {
-            result.add("");
-            return result;
-        }
-        process(n,result,"",0,0);
-        return result;
-    }
-    public void process(int n, List<String> result, String tmp, int left, int right){
-        if(left == right && left == n) {
-            result.add(tmp);
-            return;
-        }
-        if(left > n || right > left) return;
-        process(n,result, tmp + "(", left+1, right);
-        process(n,result, tmp + ")", left, right+1);
-    }
-}
-
-===============================
-//I feel above solution a little bit hard to understand, try to understand the second, then you can understand first
-//https://leetcode.com/problems/generate-parentheses/discuss/10100/Easy-to-understand-Java-backtracking-solution
-    class Solution {
+class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
         if(n <= 0) return result;
-        process(0,0,"",result,n);
+        helper(n, result, 0, 0, "");
         return result;
     }
     
-    public void process(int left, int right, String tmp, List<String> result, int n){
-        if(left >= n && left == right) {
+    public void helper(int n, List<String> result, int left, int right, String tmp){
+        if(left == n && right == n) {
             result.add(tmp);
+            return;
         }
-        if(left < n) {
-            process(left + 1, right, tmp + "(", result, n);
-        }
-        if(right < left) {
-            process(left, right + 1, tmp + ")", result, n);   
-        }
+        if(left > n || right > n || right > left) return;
+        helper(n, result, left + 1, right, tmp + "(");
+        helper(n, result, left, right + 1, tmp + ")");
     }
 }
