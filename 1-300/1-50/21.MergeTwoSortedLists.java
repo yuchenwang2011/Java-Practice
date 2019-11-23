683fcb780fdc79d5ae73dad63dd3ebec4dce118e
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-11-23 17:54:38 -0500

    Update 21.MergeTwoSortedLists.java

--- 1-300/1-50/21.MergeTwoSortedLists.java
+++ 1-300/1-50/21.MergeTwoSortedLists.java
@@ -35,26 +35,9 @@ class Solution {
     }
 }
 
-//This answer uses recursion, so brilliant!
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-      //This will use recursion.
-      //https://leetcode.com/discuss/77201/java-recursive-solution-in-6-lines
-      //https://leetcode.com/discuss/53608/simple-recursive-java-solution
-      if(l1 == null) return l2;
-      if(l2 == null) return l1;
-      ListNode head = l1.val < l2.val? l1:l2;
-      ListNode nonhead = l1.val < l2.val? l2:l1;
-      head.next = mergeTwoLists(head.next,nonhead);
-      return head;
-    }
-}
-
-//This method you have to 想象这是一串葡萄，每次返回后面的一串
-public class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-      if(l1 == null) return l2;
-      if(l2 == null) return l1;
+      if(l1 == null || l2 == null) return l1 == null ? l2 : l1;
       if(l1.val <= l2.val){
           l1.next = mergeTwoLists(l1.next,l2);
           return l1;
