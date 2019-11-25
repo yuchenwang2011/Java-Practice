dec8bb80af2deb333645a449a9038c66ba06bfbf
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-11-24 19:23:28 -0500

    Update 31.NextPermutation.java

--- 1-300/1-50/31.NextPermutation.java
+++ 1-300/1-50/31.NextPermutation.java
@@ -15,94 +15,34 @@ and its corresponding outputs are in the right-hand column.
 1,1,5 → 1,5,1
 
 Answer:
-public class Solution {
-    public void nextPermutation(int[] nums) {
-       //I was inspired by this solution:
-       //https://leetcode.com/discuss/72050/readable-code-without-confusing-i-j-and-with-explanation
-       //0125330-->0135320-->0130235
-       //https://github.com/yuchenwang2011/Java-Practice/blob/master/31.NextPermutation.png
-       if(nums == null || nums.length ==0){
-           return;
-       }
-       int start = 0; boolean possible = false;
-       for(int i = nums.length-1; i >=0 ; i--){
-           if(i > 0 && nums[i-1] >= nums[i]){
-               continue;
-           } else if (i> 0 && nums[i-1] < nums[i]) {
-               start = i-1;
-               possible = true;
-               break;
-           }
-       }
-       int end = nums.length-1;
-       if(possible == true){
-         for(int i = nums.length-1; i > start; i--){
-             if(nums[i] > nums[start]){
-                 end = i;
-                 break;
-             }
-         }
-         int tmp = nums[start];
-         nums[start] = nums[end];
-         nums[end] = tmp;
-         start++; //for next reverse step
-         end = nums.length-1;
-       }
-       //Now all the elements after nonDescentElementIdx are decreasing order, just reverse them
-       while(start <= end){
-           int tmp = nums[start];
-           nums[start] = nums[end];
-           nums[end] = tmp;
-           start++;end--;
-       }
-    }
-}
-
-
-
-=========================second round my own solution according to same inspiration==================
- class Solution {
+//0125330-->0135320-->0130235
+//https://github.com/yuchenwang2011/Java-Practice/blob/master/31.NextPermutation.png
+class Solution {
     public void nextPermutation(int[] nums) {
         if(nums == null || nums.length == 0) return;
-        
-        int start = findUnscendingNum(nums);
-        if(start == 0) {
-            reverseArray(0, nums);
-            return;
-        }
-        findSmallestBiggerThanPivot(start, nums);
-        reverseArray(start, nums);
-    }
-    
-    public void findSmallestBiggerThanPivot(int start, int[] nums){
-        int tmp = nums[start - 1];
-        int smallestBiggerThanTmp = start;
-        while(smallestBiggerThanTmp < nums.length){
-            if(nums[smallestBiggerThanTmp] <= tmp) {
-                smallestBiggerThanTmp--;
+        boolean possible = false;
+        int start = 0;
+        for(int i = nums.length - 1; i > 0; i--){
+            if(nums[i - 1] < nums[i]) {
+                possible = true;
+                start = i - 1;
                 break;
             }
-            smallestBiggerThanTmp++;
         }
-        if(smallestBiggerThanTmp == nums.length) smallestBiggerThanTmp--;
-        nums[start - 1] = nums[smallestBiggerThanTmp];
-        nums[smallestBiggerThanTmp] = tmp;
-    }
-    
-    public int findUnscendingNum(int[] nums){
-        for(int i = 0; i < nums.length; i++){
-            int j = i;
-            while(j < nums.length){
-                if(j > i && nums[j] > nums[j - 1]) break;
-                j++;
+        if(possible) {
+            for(int i = nums.length - 1; i >= start; i--){
+                if(nums[i] > nums[start]) {
+                    int tmp = nums[start];
+                    nums[start] = nums[i];
+                    nums[i] = tmp;
+                    start++;
+                }
             }
-            if(j == nums.length) return i;
         }
-        return nums.length - 1;
+        reverseArray(nums, start, nums.length - 1);
     }
     
-    public void reverseArray(int start, int[] nums){
-        int end = nums.length - 1;
+    public void reverseArray(int[] nums, int start, int end){
         while(start < end){
             int tmp = nums[start];
             nums[start] = nums[end];
