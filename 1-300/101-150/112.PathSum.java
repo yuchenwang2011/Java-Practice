c376e040755c6d117f508e78694fb0eefa23c742
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-12-08 17:07:00 -0500

    Update 112.PathSum.java

--- 1-300/101-150/112.PathSum.java
+++ 1-300/101-150/112.PathSum.java
@@ -37,51 +37,19 @@ public class Solution {
 class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if(root == null) return false;
-        
-        Stack<TreeNode> nodeStack = new Stack<>();
-        Stack<Integer> sumStack = new Stack<>();
-        
-        nodeStack.push(root);
-        sumStack.push(sum);
-        
-        while(!nodeStack.isEmpty()){
-            TreeNode node = nodeStack.pop();
-            int currentSum = sumStack.pop();
-            if(node.left == null && node.right == null && node.val == currentSum) return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            root = queue.poll();
+            if(root.left == null && root.right == null && root.val == sum) return true;
             
-            if(node.right != null) {
-                nodeStack.push(node.right);
-                sumStack.push(currentSum - node.val);
+            if(root.left != null) {
+                root.left.val += root.val;
+                queue.offer(root.left);
             }
-            
-            if(node.left != null) {
-                nodeStack.push(node.left);
-                sumStack.push(currentSum - node.val);
-            }
-        }
-        
-        return false;
-    }
-}
-
-
-class Solution {
-    public boolean hasPathSum(TreeNode root, int sum) {
-        if(root == null) return false;
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        while(!stack.isEmpty()){
-            TreeNode node = stack.pop();
-            if(node.left == null && node.right == null && node.val == sum) return true;
-            
-            if(node.right != null) {
-                node.right.val += node.val;
-                stack.push(node.right);
-            }
-            
-            if(node.left != null) {
-                node.left.val += node.val;
-                stack.push(node.left);
+            if(root.right != null) {
+                root.right.val += root.val;
+                queue.offer(root.right);
             }
         }
         return false;
