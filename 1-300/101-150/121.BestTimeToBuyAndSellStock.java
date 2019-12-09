7fc2ae9294f7fa2d5090d322042bb4f762ae6894
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-12-08 23:20:54 -0500

    Update 121.BestTimeToBuyAndSellStock.java

--- 1-300/101-150/121.BestTimeToBuyAndSellStock.java
+++ 1-300/101-150/121.BestTimeToBuyAndSellStock.java
@@ -5,27 +5,6 @@ If you were only permitted to complete at most one transaction
 (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 
 Answer: 
-public class Solution {
-    //this question is very easy to implement but hard to understand
-    //why we bought on first day and then find second day cheaper, then change our cost to second day's price
-    //and we won't know if there will be a higher price tomorror to make more profit
-    public int maxProfit(int[] prices) {
-        if(prices == null || prices.length ==0 || prices.length ==1) {
-            return 0; //as no buy and no sell is profit 0
-        }
-        int min = prices[0]; int profit = 0;
-        for (int i = 0 ;  i < prices.length ; i++) {
-            if (prices[i] < min) {
-                min = prices[i];  //find the minimum price
-            } else {
-                profit = Math.max(profit, prices[i]-min);
-            }
-        }
-        return profit;
-    }
-}
-
-//a little simplied above solution
 class Solution {
     public int maxProfit(int[] prices) {
         int result = 0;
