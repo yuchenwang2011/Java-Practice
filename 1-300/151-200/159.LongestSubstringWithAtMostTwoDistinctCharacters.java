8a052e6a4e7776dfd24c760375eabebe0737a5df
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-12-11 20:50:30 -0500

    Update 159.LongestSubstringWithAtMostTwoDistinctCharacters.java

--- 1-300/151-200/159.LongestSubstringWithAtMostTwoDistinctCharacters.java
+++ 1-300/151-200/159.LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -1,17 +1,19 @@
 159. Longest Substring with At Most Two Distinct Characters
-Hard
-Given a string s , find the length of the longest substring t  that contains at most 2 distinct characters.
+Medium
+
+Given a string s , find the length of the longest substring t that contains at most 2 distinct characters.
 
 Example 1:
 Input: "eceba"
 Output: 3
 Explanation: t is "ece" which its length is 3.
-
+    
 Example 2:
 Input: "ccaabbb"
 Output: 5
 Explanation: t is "aabbb" which its length is 5.
-
+Accepted 86,501 Submissions 179,089
+    
 Answer:
 //跟着例子走一遍就行
 class Solution {
