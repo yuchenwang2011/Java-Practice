d4e3141541c10550d66cca0c8caf8cf840f3a85d
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-12-17 00:53:41 -0500

    Update 276.PaintFence.java

--- 1-300/251-300/276.PaintFence.java
+++ 1-300/251-300/276.PaintFence.java
@@ -28,19 +28,18 @@ public class Solution {
     }
 }
 
-public class Solution {
+class Solution {
     public int numWays(int n, int k) {
-        if(n == 0 || k ==0) return 0;
-        if (n == 1) return k;
-        
+        if(n <= 0 || k <= 0) return 0;
+        if(n == 1) return k;
+        if(n == 2) return k * k;
         int first = k;
         int second = k * k;
-        int result = 0;
-        for(int i = 2; i < n; i++){
-            result = first * (k - 1) + second * 1 * (k - 1);
+        for(int i = 3; i <= n; i++){
+            int third = first * (k - 1) * 1 + second * (k - 1);
             first = second;
-            second = result;
+            second = third;
         }
-        return result;
+        return second;
     }
 }
