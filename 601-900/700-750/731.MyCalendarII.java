998b8b76bbd0cee0a36bb6cbdf611b12774cafe1
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-12-19 12:47:33 -0500

    Update 731.MyCalendarII.java

--- 601-900/700-750/731.MyCalendarII.java
+++ 601-900/700-750/731.MyCalendarII.java
@@ -76,3 +76,31 @@ class MyCalendarTwo {
         }
     }
 }
+
+                                                       
+                                                       class MyCalendarTwo {
+    private List<int[]> bookings;
+    private List<int[]> overlaps;
+    
+    public MyCalendarTwo() {
+        bookings = new ArrayList<>();
+        overlaps = new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        for (int[] bo: bookings) {
+            int overlapStart = Math.max(bo[0], start), overlapEnd = Math.min(bo[1], end);
+            if (overlapStart < overlapEnd) {
+                for (int[] ov: overlaps) {
+                    if (Math.max(ov[0], overlapStart) < Math.min(ov[1], overlapEnd)) {
+                        overlaps.clear();
+                        return false;
+                    }
+                }
+                overlaps.add(new int[] {overlapStart, overlapEnd});
+            }
+        } 
+        bookings.add(new int[] {start, end});
+        return true;
+    }
+}
