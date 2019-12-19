940d639a749d618f293d7a391c1bf7eb58b82910
yuchenwang2011 <yuchen.wang2011@gmail.com>
2019-12-19 14:27:21 -0500

    Update 787.CheapestFlightsWithinKStops.java

--- 601-900/751-800/787.CheapestFlightsWithinKStops.java
+++ 601-900/751-800/787.CheapestFlightsWithinKStops.java
@@ -37,32 +37,35 @@ Answer:
 //this question DFS + BFS, where BFS is much faster than DFS.
 //DFS solution
 class Solution {
-    int result = Integer.MAX_VALUE;
+    int result;
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-        if(n <= 0 || flights == null || flights.length <= 0 || K < 0) return -1;
-        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        result = Integer.MAX_VALUE;
+        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
         for(int[] flight : flights){
-            if(!map.containsKey(flight[0])) map.put(flight[0], new HashMap<>());
-            map.get(flight[0]).put(flight[1], flight[2]);
+            int a = flight[0];
+            int b = flight[1];
+            int c = flight[2];
+            graph.putIfAbsent(a, new HashMap<>());
+            graph.get(a).put(b, c);
         }
-    
-        helper(map, src, dst, K, 0);
+        helper(graph, src, dst, K + 1, 0);
         return result == Integer.MAX_VALUE ? -1 : result;
     }
     
-    public void helper(Map<Integer, Map<Integer, Integer>> graph, int src, int dst, int k, int cost){
+    public void helper(Map<Integer, Map<Integer, Integer>> graph, int src, int dst, int K, int cost){
+        if(K < 0) return;
         if(src == dst) {
-            if(cost <= result) result = cost;
-            return;
+            if(cost < result) {
+                result = cost;
+                return;
+            }
         }
-        if(k < 0 || src == dst) return;
-        
-        Map<Integer, Integer> map = graph.get(src);
-        if(map == null || map.size() == 0) return;
-        for(int stop : map.keySet()){
-            int newCost = cost + map.get(stop);
-            if(newCost > result) continue; 
-            helper(graph, stop, dst, k - 1, newCost);
+        if(!graph.containsKey(src)) return;
+        Map<Integer, Integer> nexts = graph.get(src);
+        for(int next : nexts.keySet()){
+            int newCost = cost + nexts.get(next);
+            if(newCost > result) continue;
+            helper(graph, next, dst, K - 1, newCost);
         }
     }
 }
@@ -70,35 +73,41 @@ class Solution {
 //BFS
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        int result = Integer.MAX_VALUE;
+        
+        Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
         for(int[] flight : flights){
-            if(!map.containsKey(flight[0])) map.put(flight[0], new HashMap<>());
-            map.get(flight[0]).put(flight[1], flight[2]);
+            int a = flight[0];
+            int b = flight[1];
+            int c = flight[2];
+            graph.putIfAbsent(a, new HashMap<>());
+            graph.get(a).put(b, c);
         }
         
-        int result = Integer.MAX_VALUE;
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{src, 0});
+        K = K + 1;
         while(!queue.isEmpty()){
             int size = queue.size();
+            if(K < 0) break;
             for(int i = 0; i < size; i++){
-                int[] stop = queue.poll();
-                if(stop[1] > result) continue;
-                if(!map.containsKey(stop[0])) continue;
-                Map<Integer, Integer> neighbours = map.get(stop[0]);
-                for(int next : neighbours.keySet()){
-                    int cost = neighbours.get(next) + stop[1];
-                    if(next == dst) {
-                        result = Math.min(result, cost);
+                int[] flight = queue.poll();
+                if(flight[0] == dst) {
+                    if(flight[1] < result) {
+                        result = flight[1];
                         continue;
                     }
-                    queue.offer(new int[]{next, cost});
+                }
+                if(!graph.containsKey(flight[0])) continue;
+                Map<Integer, Integer> nexts = graph.get(flight[0]);
+                for(int next : nexts.keySet()){
+                    int newCost = flight[1] + nexts.get(next);
+                    if(newCost > result) continue;
+                    queue.offer(new int[]{next, newCost});
                 }
             }
             K--;
-            if(K == -1) break; 
         }
-        
-        return result == Integer.MAX_VALUE ? -1 : result;
+        return result == Integer.MAX_VALUE? -1 : result;
     }
 }
