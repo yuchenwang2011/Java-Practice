d0b104c0cb0be54ee44666187bfa5e1a5dcfb1ac
yuchenwang2011 <yuchen.wang2011@gmail.com>
2020-01-20 18:58:30 -0500

    Update 338.CountingBits.java

--- 301-600/301-350/338.CountingBits.java
+++ 301-600/301-350/338.CountingBits.java
@@ -20,21 +20,17 @@ Divide the numbers in ranges like [2-3], [4-7], [8-15] and so on. And try to gen
 Or does the odd/even status of the number help you in calculating the number of 1s?
 
 Answer:
-public class Solution {
-    //Got inspired by this answer:
-    //https://leetcode.com/discuss/92609/three-line-java-solution
+//From my understanding, when shift right a bit, 1111 -> 111, you just need to know how many bits 111 has
+//then plus if the shifted rightmost big whether is 1
+//One more example, 1110 -> 111, you just need to know how many bits 111 has, then plus 
+//whether the shifted right most bit whether is 1
+class Solution {
     public int[] countBits(int num) {
-        if(num <= 0) return new int[]{0};
-        int[] dp = new int[num+1];
+        if(num < 0) return new int[0];
+        int[] dp = new int[num + 1];
         dp[0] = 0;
-        dp[1] = 1;
- 
-        //From my understanding, when shift right a bit, 1111 -> 111, you just need to know how many bits 111 has
-        //then plus if the shifted rightmost big whether is 1
-        //One more example, 1110 -> 111, you just need to know how many bits 111 has, then plus 
-        //whether the shifted right most bit whether is 1
-        for(int i = 2; i <= num; i++){
-            dp[i] = dp[i >> 1] + (i & 1);   
+        for(int i = 1; i < dp.length; i++){
+            dp[i] = dp[i >> 1] + (i & 1);
         }
         return dp;
     }
