ed4cc11830d1a42bf41ef9944fa6000e17d6a672
yuchenwang2011 <yuchen.wang2011@gmail.com>
2020-01-20 23:12:49 -0500

    Update 342.PowerOfFour.java

--- 301-600/301-350/342.PowerOfFour.java
+++ 301-600/301-350/342.PowerOfFour.java
@@ -17,6 +17,7 @@ class Solution {
 public class Solution {
     public boolean isPowerOfFour(int num) {
         if(num <= 0) return false;
+        //it is 01010101010101010101010101010101 in binary with a length of 32
         return ((num & (num - 1)) == 0) && ((num & 0x55555555) == num); 
     }
 }
