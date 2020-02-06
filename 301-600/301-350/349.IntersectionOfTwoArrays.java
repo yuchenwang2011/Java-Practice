6dcd1467847e3ce0ab292b83df60cac7a46ad6aa
yuchenwang2011 <yuchen.wang2011@gmail.com>
2020-02-05 23:11:03 -0500

    Update 349.IntersectionOfTwoArrays.java

--- 301-600/301-350/349.IntersectionOfTwoArrays.java
+++ 301-600/301-350/349.IntersectionOfTwoArrays.java
@@ -38,3 +38,11 @@ class Solution {
         return result;
     }
 }
+
+//java8 stream
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+        return Arrays.stream(nums1).distinct().filter(set::contains).toArray();
+    }
+}
