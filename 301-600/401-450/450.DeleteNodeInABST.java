c40ef3649a41ef91c704e185e2e7ac58297a7e9a
yuchenwang2011 <yuchen.wang2011@gmail.com>
2020-02-16 11:36:21 -0500

    Update 450.DeleteNodeInABST.java

--- 301-600/401-450/450.DeleteNodeInABST.java
+++ 301-600/401-450/450.DeleteNodeInABST.java
@@ -39,33 +39,29 @@ Another valid answer is [5,2,6,null,4,null,7].
 Accepted 91,374 Submissions 219,262
 
 //两个解法
-public TreeNode deleteNode(TreeNode root, int key) {
-    if(root == null){
-        return null;
-    }
-    if(key < root.val){
-        root.left = deleteNode(root.left, key);
-    }else if(key > root.val){
-        root.right = deleteNode(root.right, key);
-    }else{
-        if(root.left == null){
-            return root.right;
-        }else if(root.right == null){
-            return root.left;
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return root;
+        if(key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val){
+            root.right = deleteNode(root.right, key);
+        } else {
+            if(root.left == null) return root.right;
+            if(root.right == null) return root.left;
+            TreeNode min = findMin(root.right);
+            root.val = min.val;
+            root.right = deleteNode(root.right, min.val);
         }
-        
-        TreeNode minNode = findMin(root.right);
-        root.val = minNode.val;
-        root.right = deleteNode(root.right, root.val);
+        return root;
     }
-    return root;
-}
-
-private TreeNode findMin(TreeNode node){
-    while(node.left != null){
-        node = node.left;
+    
+    public TreeNode findMin(TreeNode node){
+        while(node.left != null){
+            node = node.left;
+        }
+        return node;
     }
-    return node;
 }
 
 
