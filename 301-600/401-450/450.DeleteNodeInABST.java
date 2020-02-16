cd8aacf96f0d16687baa560634baea71ba60d513
yuchenwang2011 <yuchen.wang2011@gmail.com>
2020-02-16 11:47:21 -0500

    Update 450.DeleteNodeInABST.java

--- 301-600/401-450/450.DeleteNodeInABST.java
+++ 301-600/401-450/450.DeleteNodeInABST.java
@@ -66,21 +66,24 @@ class Solution {
 
 
 ==========2nd=====
-public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) return null;
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return root;
         
-        if (root.val > key) {
-            root.left = deleteNode(root.left, key);
-        } else if (root.val < key) {
+        if(root.val < key) {
             root.right = deleteNode(root.right, key);
+        } else if (root.val > key){
+            root.left = deleteNode(root.left, key);
         } else {
-            if (root.left == null) return root.right;
-            if (root.right == null) return root.left;
-            
-            TreeNode rightSmallest = root.right;
-            while (rightSmallest.left != null) rightSmallest = rightSmallest.left;
-            rightSmallest.left = root.left;
+            if(root.left == null) return root.right;
+            if(root.right == null) return root.left;
+            TreeNode node = root.right;
+            while(node.left != null){
+                node = node.left;
+            }
+            node.left = root.left;
             return root.right;
         }
         return root;
+    }
 }
