f37c8fe4a7e3a01a89c0cf90d3aa5528159ea2ae
yuchenwang2011 <yuchen.wang2011@gmail.com>
2022-03-07 18:09:01 -0800

    Update 44.WildcardMatching.java

--- 1-300/1-50/44.WildcardMatching.java
+++ 1-300/1-50/44.WildcardMatching.java
@@ -1,4 +1,4 @@
-Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+Hard: Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
 
 '?' Matches any single character.
 '*' Matches any sequence of characters (including the empty sequence).
