5dc718070c82451a3b199a123dbb65001e17d093
yuchenwang2011 <yuchen.wang2011@gmail.com>
2022-03-31 18:42:45 -0700

    Update 228.SummaryRanges.java

--- 1-300/201-250/228.SummaryRanges.java
+++ 1-300/201-250/228.SummaryRanges.java
@@ -10,23 +10,28 @@ public class Solution {
     //**********Usage of List: http://stackoverflow.com/questions/13395114/how-to-initialize-liststring-object-in-java
     //**********Remember the syntax of Interger.toString(i)
     public List<String> summaryRanges(int[] nums) {
-        List<String> result = new ArrayList<String>();
-        if(nums.length == 0 || nums == null){
-            return result;
+        List<String> result = new ArrayList<>();
+        if(nums == null || nums.length == 0) return result;
+        
+        int idx = 0;
+        for(int i = 0; i < nums.length; i++){
+            while(i + 1 < nums.length && nums[i + 1] == nums[i] + 1){
+                i++;
+            }
+            
+            if(idx == i){
+                //means no connected int, such as 1, 2, but 1,3
+                //so result should be 1
+                result.add(nums[i] + "");
+            } else {
+                //means there are a connected int, 1, 2, 3, 4
+                //so result should be 1 ->4
+                result.add(nums[idx] + "->" + nums[i]);
+            }
+            
+            idx = i + 1;
         }
-
-       int start = 0;
-       for(int i =0; i < nums.length ; i++){
-          start = i;
-          while (i+1 < nums.length && nums[i+1] == nums[i]+1) {
-              i++;
-          }
-          if(start == i) {
-              result.add(nums[start] + "");
-          } else {
-              result.add(nums[start] + "->" + nums[i]);
-          }
-       }
-       return result;
+        
+        return result;
     }
 }
