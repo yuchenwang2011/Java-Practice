c52bf7cae7cbca1ceec2da2219df787c1d668532
yuchenwang2011 <yuchen.wang2011@gmail.com>
2023-09-09 23:15:35 -0700

    Update 399.EvaluateDivision.java

--- 301-600/351-400/399.EvaluateDivision.java
+++ 301-600/351-400/399.EvaluateDivision.java
@@ -45,7 +45,10 @@ class Solution {
     }
     
     public double helper(Map<String , Map<String, Double>> graph, String a, String b, double val, Set<String> set){
-        if(!graph.containsKey(a) || !set.add(a)) return -1; //这一行必须放前面
+        if(!graph.containsKey(a)) return -1; //这一行比如说之前graph里都是abc，结果query想求xyz
+        if(!set.add(a)) return -1; //你想啊，第一个node肯定是没visited，它去循环第二个的话，肯定又没有visited。后来乱起来了以后，发现visited，那后面就不用再计算了，直接就return不算了
+        //至于这里return的-1，其实不是想要它的值，而是因为必须return，所以就return一个-1。因为最后还会对结果检查是不是-1，不是-1的话，那就说明有正确答案。
+        
         if(a.equals(b)) return val;
         Map<String, Double> map = graph.get(a);
         for(String x : map.keySet()){
