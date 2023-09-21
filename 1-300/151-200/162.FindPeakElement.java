1b81e8da1a4b50c1ed10458088bfa33b8f8ff4e2
yuchenwang2011 <yuchen.wang2011@gmail.com>
2023-09-20 21:28:58 -0700

    Update 162.FindPeakElement.java

--- 1-300/151-200/162.FindPeakElement.java
+++ 1-300/151-200/162.FindPeakElement.java
@@ -37,3 +37,19 @@ public class Solution {
         return nums[start] < nums[end] ? end : start;
     }
 }
+
+//左闭右开的写法的简化版
+//comment in https://leetcode.com/problems/find-peak-element/solutions/50239/java-solution-and-explanation-using-invariants
+public int findPeakElement(int[] nums) {
+    int left = 0;
+    int right = nums.length - 1;
+    while(left < right) {
+        int mid = left + (right - left) / 2;
+        if(nums[mid] < nums[mid + 1]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return left;
+}
