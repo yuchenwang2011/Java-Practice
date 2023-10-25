1ebd35da007e777a260503d998efd184be59c001
yuchenwang2011 <yuchen.wang2011@gmail.com>
2023-10-24 22:00:09 -0700

    Update 438.FindAllAnagramsInAString.java

--- 301-600/401-450/438.FindAllAnagramsInAString.java
+++ 301-600/401-450/438.FindAllAnagramsInAString.java
@@ -25,3 +25,44 @@ s and p consist of lowercase English letters.
 Accepted 810.1K Submissions 1.6M Acceptance Rate 50.5%
 
 Answer:
+//followed this answer
+//https://leetcode.com/problems/find-all-anagrams-in-a-string/solutions/92007/sliding-window-algorithm-template-to-solve-all-the-leetcode-substring-search-problem/
+public class Solution {
+    public List<Integer> findAnagrams(String s, String t) {
+        List<Integer> result = new LinkedList<>();
+        if(t.length()> s.length()) return result;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int counter = map.size();
+        
+        int begin = 0, end = 0;
+        int head = 0;
+        int len = Integer.MAX_VALUE;
+        
+        
+        while(end < s.length()){
+            char c = s.charAt(end);
+            if( map.containsKey(c) ){
+                map.put(c, map.get(c)-1);
+                if(map.get(c) == 0) counter--;
+            }
+            end++;
+            
+            while(counter == 0){
+                char tempc = s.charAt(begin);
+                if(map.containsKey(tempc)){
+                if(map.get(tempc) == 0) counter++;
+                map.put(tempc, map.get(tempc) + 1);
+                }
+                if(end-begin == t.length()){
+                    result.add(begin);
+                }
+                begin++;
+            }
+            
+        }
+        return result;
+    }
+}
