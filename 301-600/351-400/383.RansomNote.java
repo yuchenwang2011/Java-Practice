5efa501186a004bae40f613ff718d342f75bbfcd
yuchenwang2011 <yuchen.wang2011@gmail.com>
2023-11-27 22:28:01 -0800

    Create 383.RansomNote.java

---
+++ 301-600/351-400/383.RansomNote.java
@@ -0,0 +1,24 @@
+383. Ransom Note
+Easy 4.7K 471
+Given two strings ransomNote and magazine, return true if ransomNote 
+can be constructed by using the letters from magazine and false otherwise.
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Example 2:
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Example 3:
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ 
+Constraints:
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters.
+Accepted 960.8K Submissions 1.6M Acceptance Rate 60.0%
+
+Answer:
