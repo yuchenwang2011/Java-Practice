75e1b7290872cebf4f093b2e1083831c7673e796
yuchenwang2011 <yuchen.wang2011@gmail.com>
2023-12-06 22:01:38 -0800

    Update 438.FindAllAnagramsInAString.java

--- 301-600/401-450/438.FindAllAnagramsInAString.java
+++ 301-600/401-450/438.FindAllAnagramsInAString.java
@@ -1,8 +1,10 @@
 438. Find All Anagrams in a String
 Medium 11.9K 321
 
-Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. 
+ You may return the answer in any order.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+ typically using all the original letters exactly once.
 
 Example 1:
 Input: s = "cbaebabacd", p = "abc"
