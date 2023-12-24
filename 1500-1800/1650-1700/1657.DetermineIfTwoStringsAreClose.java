d1065fda74cce85081dfb9426880ae7f31361657
yuchenwang2011 <yuchen.wang2011@gmail.com>
2023-12-23 17:23:30 -0800

    Update 1657.DetermineIfTwoStringsAreClose.java

--- 1500-1800/1650-1700/1657.DetermineIfTwoStringsAreClose.java
+++ 1500-1800/1650-1700/1657.DetermineIfTwoStringsAreClose.java
@@ -4,7 +4,8 @@ Medium 2.6K 134
 Two strings are considered close if you can attain one from the other using the following operations:
 Operation 1: Swap any two existing characters.
 For example, abcde -> aecdb
-Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+Operation 2: Transform every occurrence of one existing character into another existing character, 
+ and do the same with the other character.
 For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
 You can use the operations on either string as many times as necessary.
 Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
@@ -53,7 +54,7 @@ class Solution {
         for(char c : word2.toCharArray()){
             array2[c - 'a']++;
         }
-        // if one has a letter which another one doesn't have, dont exist
+        // 起码基本的字母得一样
         for(int i = 0; i < 26; i++){
             if(array1[i] == array2[i]){
                 continue;
